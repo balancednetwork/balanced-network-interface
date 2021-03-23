0fbe0449dbd60e3d130c2ef2c902fc8934f429d7
Nguyen Viet Huan <ngnviethuan@gmail.com>
2021-03-23 23:54:12 +0700

    Update ratio ICX and USD from ws

--- src/app/containers/HomePage/index.tsx
+++ src/app/containers/HomePage/index.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
 
 import BigNumber from 'bignumber.js';
-import { BalancedJs } from 'packages/BalancedJs';
+import { AccountType, BalancedJs } from 'packages/BalancedJs';
 import { useIconReact } from 'packages/icon-react';
 import { convertLoopToIcx } from 'packages/icon-react/utils';
 import { Helmet } from 'react-helmet-async';
 import styled from 'styled-components';
+import { w3cwebsocket as W3CWebSocket } from 'websocket';
 
 import CollateralPanel from 'app/components/home/CollateralPanel';
 import LoanPanel from 'app/components/home/LoanPanel';
@@ -64,34 +65,62 @@ export function usePrice() {
   }, PERIOD);
 }
 
-export function useBalance(account?: string | null) {
+export function useBalance(account?: AccountType) {
   // eject this account and we don't need to account params for when call contract
   bnJs.eject({ account });
 
   const changeBalanceValue = useChangeWalletBalance();
 
+  const changeRatioValue = useChangeRatio();
+
   const transactions = useAllTransactions();
 
   const fetchBalances = React.useCallback(() => {
-    if (account) {
-      Promise.all([
-        bnJs.sICX.balanceOf(),
-        bnJs.Baln.balanceOf(),
-        bnJs.bnUSD.balanceOf(),
-        bnJs.Rewards.getRewards(),
-      ]).then(result => {
+    if (!account) return;
+
+    Promise.all([bnJs.sICX.balanceOf(), bnJs.Baln.balanceOf(), bnJs.bnUSD.balanceOf(), bnJs.Rewards.getRewards()]).then(
+      result => {
         const [sICXbalance, BALNbalance, bnUSDbalance, BALNreward] = result.map(v => convertLoopToIcx(v as BigNumber));
         changeBalanceValue({ sICXbalance });
         changeBalanceValue({ BALNbalance });
         changeBalanceValue({ bnUSDbalance });
         changeBalanceValue({ BALNreward });
-      });
-    }
+      },
+    );
   }, [account, changeBalanceValue]);
+
+  const initWebSocket = React.useCallback(() => {
+    if (!account) return;
+
+    const client = new W3CWebSocket(`ws://localhost:8069/ws/address/${account}`);
+    client.onopen = () => {
+      client.send(
+        JSON.stringify({
+          address: account,
+        }),
+      );
+
+      client.onmessage = (msgEvent: any) => {
+        const data = JSON.parse(msgEvent.data);
+
+        const { type, data: payload } = data;
+        if (type === 'band_get_reference_data') {
+          const { price } = payload;
+          changeRatioValue({ ICXUSDratio: new BigNumber(price / 10 ** 18) });
+        }
+
+        if (type === 'MethodCall') {
+          fetchBalances();
+          alert(`https://bicon.tracker.solidwallet.io/transaction/${JSON.stringify(data, null, 2)}`);
+        }
+      };
+    };
+  }, [account, fetchBalances, changeRatioValue]);
 
   React.useEffect(() => {
     fetchBalances();
-  }, [fetchBalances, transactions]);
+    initWebSocket();
+  }, [fetchBalances, initWebSocket, transactions]);
 }
 
 export function useCollateralInfo(account?: string | null) {

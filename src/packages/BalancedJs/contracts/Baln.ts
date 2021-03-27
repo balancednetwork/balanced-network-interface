f60255c57623f9d79844fe122982dffc510128af
thach4567 <thach4567@gmail.com>
2021-03-27 17:55:03 +0700

    implemented swap token on balanced interface.

--- src/packages/BalancedJs/contracts/Baln.ts
+++ src/packages/BalancedJs/contracts/Baln.ts
@@ -1,3 +1,6 @@
+import { IconAmount } from 'icon-sdk-js';
+
+import { ResponseJsonRPCPayload } from '..';
 import addresses from '../addresses';
 import ContractSettings from '../contractSettings';
 import { Contract } from './contract';
@@ -29,5 +32,28 @@ export default class Baln extends Contract {
     });
 
     return this.call(callParams);
+  }
+
+  async swapToBnUSD(value: number, slippage: string): Promise<ResponseJsonRPCPayload> {
+    const data =
+      '0x' +
+      Buffer.from(
+        '{"method": "_swap", "params": {"toToken":"' +
+          addresses[this.nid].bnUSD +
+          '", "maxSlippage":' +
+          slippage +
+          '}}',
+        'utf8',
+      ).toString('hex');
+    const valueHex = '0x' + IconAmount.of(value, IconAmount.Unit.ICX).toLoop().toString(16);
+    const params = { _to: addresses[this.nid].dex, _value: valueHex, _data: data };
+
+    const payload = this.transactionParamsBuilder({
+      method: 'transfer',
+      value: 0,
+      params,
+    });
+    console.log(payload);
+    return this.callIconex(payload);
   }
 }

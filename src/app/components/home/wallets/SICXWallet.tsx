56d2e4b3d31fa8f62f5600945511d86752f418e0
Wei Li <developer.clear@gmail.com>
2021-03-30 12:23:21 +0800

    add sICX feature

--- src/app/components/home/wallets/SICXWallet.tsx
+++ src/app/components/home/wallets/SICXWallet.tsx
@@ -1,40 +1,143 @@
 import React from 'react';
 
-import { Flex } from 'rebass/styled-components';
+import BigNumber from 'bignumber.js';
+import { isAddress } from 'icon-sdk-js/lib/data/Validator.js';
+import { useIconReact } from 'packages/icon-react';
+import { Flex, Box } from 'rebass/styled-components';
 
 import AddressInputPanel from 'app/components/AddressInputPanel';
-import { Button } from 'app/components/Button';
+import { Button, TextButton } from 'app/components/Button';
 import CurrencyInputPanel from 'app/components/CurrencyInputPanel';
-import { Link } from 'app/components/Link';
+import Modal from 'app/components/Modal';
 import { BoxPanel } from 'app/components/Panel';
 import { Typography } from 'app/theme';
+import bnJs from 'bnJs';
 import { CURRENCYLIST } from 'constants/currency';
+import { useTransactionAdder } from 'store/transactions/hooks';
+import { useWalletBalanceValue } from 'store/wallet/hooks';
 
-import { Grid } from './utils';
+import { Grid, MaxButton } from './utils';
 
 export default function SICXWallet() {
+  const [value, setValue] = React.useState('');
+
+  const handleCurrencyInput = (value: string) => {
+    setValue(value);
+  };
+
+  const [address, setAddress] = React.useState('');
+
+  const handleAddressInput = (value: string) => {
+    setAddress(value);
+  };
+
+  const { account } = useIconReact();
+
+  const wallet = useWalletBalanceValue();
+
+  const maxAmount = wallet.sICXbalance;
+
+  const handleMax = () => {
+    setValue(maxAmount.toFixed());
+  };
+
+  // modal logic
+  const [open, setOpen] = React.useState(false);
+
+  const toggleOpen = () => {
+    setOpen(!open);
+  };
+
+  const beforeAmount = wallet.sICXbalance;
+
+  const differenceAmount = isNaN(parseFloat(value)) ? new BigNumber(0) : new BigNumber(value);
+
+  const afterAmount = beforeAmount.minus(differenceAmount);
+
+  const addTransaction = useTransactionAdder();
+
+  const handleSend = () => {
+    bnJs
+      .eject({ account })
+      .sICX.transfer(address, differenceAmount.toNumber())
+      .then(res => {
+        if (res.result) {
+          addTransaction({ hash: res.result }, { summary: `Sent ${differenceAmount.toNumber()} sICX to ${address}.` });
+          toggleOpen();
+          setValue('');
+          setAddress('');
+        } else {
+          // to do
+          // need to handle error case
+          // for example: out of balance
+          console.error(res);
+        }
+      });
+  };
+
+  const isDisabled = !isAddress(address) || !differenceAmount.isPositive() || differenceAmount.isGreaterThan(maxAmount);
+
   return (
     <BoxPanel bg="bg3">
       <Grid>
         <Flex alignItems="center" justifyContent="space-between">
           <Typography variant="h3">Send sICX</Typography>
-          <Link href="#">Send max</Link>
+          <MaxButton onClick={handleMax}>Send max</MaxButton>
         </Flex>
 
         <CurrencyInputPanel
-          value={'0'}
+          value={value}
           showMaxButton={false}
           currency={CURRENCYLIST['sicx']}
-          onUserInput={() => null}
-          id="swap-currency-output"
+          onUserInput={handleCurrencyInput}
+          id="sicx-currency-input-in-sicx-wallet"
         />
 
-        <AddressInputPanel value="" onUserInput={() => null} />
+        <AddressInputPanel value={address} onUserInput={handleAddressInput} />
       </Grid>
 
       <Flex alignItems="center" justifyContent="center" mt={5}>
-        <Button>Send</Button>
+        <Button onClick={toggleOpen} disabled={isDisabled}>
+          Send
+        </Button>
       </Flex>
+
+      <Modal isOpen={open} onDismiss={toggleOpen}>
+        <Flex flexDirection="column" alignItems="stretch" m={5} width="100%">
+          <Typography textAlign="center" mb="5px">
+            Send asset?
+          </Typography>
+
+          <Typography variant="p" fontWeight="bold" textAlign="center" fontSize={20}>
+            {differenceAmount.toFixed(2) + ' sICX'}
+          </Typography>
+
+          <Flex my={5}>
+            <Box width={1 / 2} className="border-right">
+              <Typography textAlign="center">Before</Typography>
+              <Typography variant="p" textAlign="center">
+                {beforeAmount.toFixed(2) + ' sICX'}
+              </Typography>
+            </Box>
+
+            <Box width={1 / 2}>
+              <Typography textAlign="center">After</Typography>
+              <Typography variant="p" textAlign="center">
+                {afterAmount.toFixed(2) + ' sICX'}
+              </Typography>
+            </Box>
+          </Flex>
+
+          <Flex justifyContent="center" mt={4} pt={4} className="border-top">
+            <TextButton onClick={toggleOpen} fontSize={14}>
+              Cancel
+            </TextButton>
+            <Button onClick={handleSend} fontSize={14}>
+              Send
+            </Button>
+          </Flex>
+        </Flex>
+      </Modal>
     </BoxPanel>
   );
 }

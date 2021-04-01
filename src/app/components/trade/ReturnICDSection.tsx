fb274d37c8fcad3b20b1ea198ad30d32cdd79892
Wei Li <developer.clear@gmail.com>
2021-04-01 18:59:17 +0800

    refactor code

--- src/app/components/trade/ReturnICDSection.tsx
+++ src/app/components/trade/ReturnICDSection.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 
 import { useIconReact } from 'packages/icon-react';
+import ClickAwayListener from 'react-click-away-listener';
 import { useMedia } from 'react-use';
 import { Flex, Box } from 'rebass/styled-components';
 import styled from 'styled-components';
@@ -8,8 +9,9 @@ import styled from 'styled-components';
 import { Button, TextButton } from 'app/components/Button';
 import CurrencyInputPanel from 'app/components/CurrencyInputPanel';
 import Divider from 'app/components/Divider';
-import DropdownText from 'app/components/DropdownText';
+import { UnderlineTextWithArrow } from 'app/components/DropdownText';
 import Modal from 'app/components/Modal';
+import { DropdownPopper } from 'app/components/Popover';
 import { Typography } from 'app/theme';
 import bnJs from 'bnJs';
 import { CURRENCYLIST } from 'constants/currency';
@@ -52,6 +54,20 @@ const ReturnICDSection = () => {
     [account, ratio],
   );
 
+  // handle retire balance dropdown
+  const [anchor, setAnchor] = React.useState<HTMLElement | null>(null);
+
+  const arrowRef = React.useRef(null);
+
+  const handleToggleDropdown = (e: React.MouseEvent<HTMLElement>) => {
+    setAnchor(anchor ? null : arrowRef.current);
+  };
+
+  const closeDropdown = () => {
+    setAnchor(null);
+  };
+
+  //
   const below800 = useMedia('(max-width: 800px)');
 
   if (below800) {
@@ -63,6 +79,7 @@ const ReturnICDSection = () => {
   };
 
   const handleRetire = () => {
+    closeDropdown();
     setOpen(true);
   };
 
@@ -72,45 +89,52 @@ const ReturnICDSection = () => {
 
   return (
     <>
-      <DropdownText text="Retire Balanced assets">
-        <Box padding={5} bg="bg4">
-          <Grid>
-            <Typography variant="h2">Retire bnUSD</Typography>
+      <ClickAwayListener onClickAway={closeDropdown}>
+        <div>
+          <UnderlineTextWithArrow onClick={handleToggleDropdown} text="Retire Balanced assets" arrowRef={arrowRef} />
 
-            <Typography>Sell your bnUSD for ${ratio.sICXbnUSDratio?.toFixed(2)} of sICX (staked ICX).</Typography>
+          <DropdownPopper show={Boolean(anchor)} anchorEl={anchor} placement="bottom-end">
+            <Box padding={5} bg="bg4">
+              <Grid>
+                <Typography variant="h2">Retire bnUSD</Typography>
 
-            <CurrencyInputPanel
-              currency={CURRENCYLIST['bnusd']}
-              value={retireAmount}
-              onUserInput={handleTypeInput}
-              showMaxButton={false}
-              id="return-icd-input"
-              bg="bg5"
-            />
+                <Typography>Sell your bnUSD for ${ratio.sICXbnUSDratio?.toFixed(2)} of sICX (staked ICX).</Typography>
 
-            <Flex flexDirection="column" alignItems="flex-end">
-              <Typography mb={2}>Minimum: 1000 bnUSD</Typography>
-              <Typography>Wallet: {wallet.bnUSDbalance?.toFixed(2)} bnUSD</Typography>
-            </Flex>
+                <CurrencyInputPanel
+                  currency={CURRENCYLIST['bnusd']}
+                  value={retireAmount}
+                  onUserInput={handleTypeInput}
+                  showMaxButton={false}
+                  id="return-icd-input"
+                  bg="bg5"
+                />
 
-            <Divider />
+                <Flex flexDirection="column" alignItems="flex-end">
+                  <Typography mb={2}>Minimum: 1000 bnUSD</Typography>
+                  <Typography>Wallet: {wallet.bnUSDbalance?.toFixed(2)} bnUSD</Typography>
+                </Flex>
 
-            <Flex alignItems="flex-start" justifyContent="space-between">
-              <Typography variant="p">Total</Typography>
-              <Flex flexDirection="column" alignItems="flex-end">
-                <Typography variant="p">{receiveAmount} sICX</Typography>
-                <Typography variant="p" color="text1" fontSize={14}>
-                  ~ {(parseFloat(receiveAmount) * ratio.sICXICXratio?.toNumber()).toFixed(2)} ICX
-                </Typography>
+                <Divider />
+
+                <Flex alignItems="flex-start" justifyContent="space-between">
+                  <Typography variant="p">Total</Typography>
+                  <Flex flexDirection="column" alignItems="flex-end">
+                    <Typography variant="p">{receiveAmount} sICX</Typography>
+                    <Typography variant="p" color="text1" fontSize={14}>
+                      ~ {(parseFloat(receiveAmount) * ratio.sICXICXratio?.toNumber()).toFixed(2)} ICX
+                    </Typography>
+                  </Flex>
+                </Flex>
+              </Grid>
+
+              <Flex justifyContent="center" mt={5}>
+                <Button onClick={handleRetire}>Retire bnUSD</Button>
               </Flex>
-            </Flex>
-          </Grid>
+            </Box>
+          </DropdownPopper>
+        </div>
+      </ClickAwayListener>
 
-          <Flex justifyContent="center" mt={5}>
-            <Button onClick={handleRetire}>Retire bnUSD</Button>
-          </Flex>
-        </Box>
-      </DropdownText>
       <Modal isOpen={open} onDismiss={toggleOpen}>
         <Flex flexDirection="column" alignItems="stretch" m={5} width="100%">
           <Typography textAlign="center" mb="5px" as="h3" fontWeight="normal">

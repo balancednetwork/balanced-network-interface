c14c8a4480946a8b16eb9e446113886071728580
Wei Li <developer.clear@gmail.com>
2021-04-12 00:44:54 +0800

    remove contracts/index.ts

--- src/packages/BalancedJs/contracts/index.ts
+++
@@ -1,7 +0,0 @@
-import BALN from './BALN';
-import Loans from './Loans';
-import Rewards from './Rewards';
-import sICX from './sICX';
-
-// eslint-disable-next-line import/no-anonymous-default-export
-export default { Loans, BALN, sICX, Rewards };

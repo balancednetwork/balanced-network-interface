55dbbd87f756efeb6d0caeb30db2f022d64db3bc
Tuan Tran <tranletuan@outlook.com>
2021-04-15 14:39:34 +0700

    update with new fixes

--- src/store/loan/hooks.ts
+++ src/store/loan/hooks.ts
@@ -96,11 +96,11 @@ export function useLoanFetchTotalRepaid(): (interval?: string | null) => void {
         try {
           axios
             .get(
-              `https://balanced.techiast.com:8069/api/v1/loan-repaid-sum?address=${account}&symbol=bnUSD&date-preset=${interval}`,
+              `https://balanced.techiast.com:8069/api/v1/asset-redeemed-sum?address=${account}&symbol=bnUSD&date-preset=${interval}`,
             )
             .then(res => {
-              const value = res.data['loan_repaid_sum'];
-              //dispatch(changeTotalRepaid({ totalRepaid: convertLoopToIcx(new BigNumber(value)) }));
+              const value = res.data['asset_redeemed_sum'];
+              dispatch(changeTotalRepaid({ totalRepaid: BalancedJs.utils.toIcx(new BigNumber(value)) }));
             });
         } catch (e) {
           console.error(e);

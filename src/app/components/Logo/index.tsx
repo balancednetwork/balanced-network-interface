fae2f964a408a6da567938ecefc0d75898e9ce4b
Wei Li <developer.clear@gmail.com>
2021-04-19 18:29:38 +0800

    fix logo section

--- src/app/components/Logo/index.tsx
+++ src/app/components/Logo/index.tsx
@@ -1,14 +1,29 @@
 import React from 'react';
 
-import { ImageProps } from 'rebass';
+import { NavLink } from 'react-router-dom';
 import styled from 'styled-components';
 
 import LogoSrc from 'assets/images/balanced-logo.png';
 
+import { MouseoverTooltip } from '../Tooltip';
+
 const LogoImg = styled.img`
   width: 100px;
+  cursor: pointer;
+
+  ${({ theme }) => theme.mediaWidth.upToSmall`
+    width: 75px;
+  `}
 `;
 
-export default function Logo(props: Omit<ImageProps, 'src' | 'alt'>) {
-  return <LogoImg src={LogoSrc} alt="Balanced logo" {...props} />;
+export default function Logo(props) {
+  return (
+    <div {...props}>
+      <NavLink exact to="/">
+        <MouseoverTooltip text="Back to the Balanced website" placement="right">
+          <LogoImg src={LogoSrc} alt="Balanced logo" />
+        </MouseoverTooltip>
+      </NavLink>
+    </div>
+  );
 }

83d8c9bf2a1eb29e90ff9bdad5fd7029df97a4a3
Wei Li <developer.clear@gmail.com>
2021-04-25 20:31:22 +0800

    fix hard-coded network id

--- src/packages/icon-react/index.tsx
+++ src/packages/icon-react/index.tsx
@@ -15,20 +15,27 @@ export const GOVERNANCE_BASE_ADDRESS = 'cx00000000000000000000000000000000000000
 
 export const API_VERSION = IconConverter.toBigNumber(3);
 
-export const iconService = new IconService(new IconService.HttpProvider('https://bicon.net.solidwallet.io/api/v3'));
-
-export const getDefaultStepCost = async () => {
-  const getStepCostsCall = new IconBuilder.CallBuilder().to(GOVERNANCE_BASE_ADDRESS).method('getStepCosts').build();
-  const { default: defaultStepCost } = await iconService.call(getStepCostsCall).execute();
-  return defaultStepCost;
-};
-
 export enum NetworkId {
   MAINNET = 1,
   YEOUIDO = 3,
   EULJIRO = 2,
   PAGODA = 80,
 }
+
+const API_ENDPOINTS = {
+  [NetworkId.MAINNET]: 'https://ctz.solidwallet.io/api/v3',
+  [NetworkId.YEOUIDO]: 'https://bicon.net.solidwallet.io/api/v3',
+};
+
+const NETWORK_ID: number = parseInt(process.env.REACT_APP_NETWORK_ID ?? '1');
+
+const iconService = new IconService(new IconService.HttpProvider(API_ENDPOINTS[NETWORK_ID]));
+
+export const getDefaultStepCost = async () => {
+  const getStepCostsCall = new IconBuilder.CallBuilder().to(GOVERNANCE_BASE_ADDRESS).method('getStepCosts').build();
+  const { default: defaultStepCost } = await iconService.call(getStepCostsCall).execute();
+  return defaultStepCost;
+};
 
 interface ICONReactContextInterface {
   account?: string | null;
@@ -51,8 +58,6 @@ const IconReactContext = React.createContext<ICONReactContextInterface>({
   disconnect: () => null,
   networkId: NetworkId.MAINNET,
 });
-
-const NETWORK_ID: number = parseInt(process.env.REACT_APP_NETWORK_ID ?? '1');
 
 export function IconReactProvider({ children }) {
   const walletType = useWalletType();

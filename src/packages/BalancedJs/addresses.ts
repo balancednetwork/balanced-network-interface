3972483fcaf80759bb6f240189f7182b080c03ea
gedanziger <gedanziger@gmail.com>
2021-05-02 22:48:23 -0700

    update for testing

--- src/packages/BalancedJs/addresses.ts
+++ src/packages/BalancedJs/addresses.ts
@@ -22,19 +22,19 @@ const MAINNET_ADDRESSES = {
 };
 
 const YEOUIDO_ADDRESSES = {
-  loans: 'cx9ba27c7c2748161c715df5858cce9d90a0c6f481',
-  staking: 'cxa688da02bcdebfc9896fdae42b16ba22ece88d1e',
-  dividends: 'cx6d21fc244448f64dc5412950d932c9d04fc0476b',
-  reserve: 'cxaa1d2725b097ece8e3b1189857a5be1cf93dbe04',
+  loans: 'cx7aef5d8a83327a649904a919c1ec87f10ce734f9',
+  staking: 'cxe96eb3da182ba7943a97caf6d9a18065bd8b220a',
+  dividends: 'cx6f5a2e3c4e25b3b548cd1e122aa32eb32c1f6dce',
+  reserve: 'cx630e5cba5317f7f5741d9f5be0b830a96c7000c4',
   daofund: 'cxcf54974d450234223a2f59e713847185ab1ad9a8',
-  rewards: 'cx3e041d560a089f4af1e4722cda1d1625d2650162',
-  dex: 'cxda87be03ebc88b7726cb739e0869336cba836a22',
-  governance: 'cx1c84b1d5721b605e71bdb7d6e46ca7c5ed046ce8',
+  rewards: 'cx97d8e0da265f147623893cb445f05c81c2e9f814',
+  dex: 'cxaa00aa510d44076fdca14f7aebe307f2872becb0',
+  governance: 'cxb6f0f6149cda7a861c808498b5802a51b719ab4e',
   band: 'cx61a36e5d10412e03c907a507d1e8c6c3856d9964',
-  sicx: 'cxac729dee78a0937c5c51cae94383533f9f21c10b',
-  bnusd: 'cx79c38de22e9a1c995736a089060e4a4edcaf8b49',
-  baln: 'cx41e1523b109ab8ee96f7732dfbb51af8218af889',
-  bwt: 'cxeac5a08d5125a3a2f73d08fb6e099109a53b79f9',
+  sicx: 'cx8e58d166790d6292931e9d7ac764be8b4496c290',
+  bnusd: 'cxe74d7522a8835f190e6edbe07563e7ad357b1f52',
+  baln: 'cxa144ae379cf5158092028a671d0b61f9d85a9302',
+  bwt: 'cx08791ea464ad76059b4cc2663594f3c91314b2d7',
 };
 
 const addresses = {

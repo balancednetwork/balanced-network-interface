83cb9ca68a5783d71b62051fb0509aefe881c074
Alan <hieunguyen.ebox@gmail.com>
2021-06-24 22:31:49 +0700

    fix: 🐛 proxy not work

--- src/apis/constants.ts
+++ src/apis/constants.ts
@@ -1,2 +1,4 @@
 export const API_V1_ENDPOINT =
-  process.env.NODE_ENV === 'production' ? 'https://balanced.geometry.io/api/v1' : '/api/v1';
+  process.env.NODE_ENV === 'production'
+    ? 'https://balanced.geometry.io/api/v1'
+    : 'https://b.balanced.geometry.io/api/v1';

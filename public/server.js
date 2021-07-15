2fdf3459b4b1bd241e5b70af6279c63b62cfe72f
Nga <cowslips9@gmail.com>
2021-07-16 00:53:45 +0700

    fix: build script

--- public/server.js
+++ public/server.js
@@ -23,6 +23,4 @@ app.get('*', (req, res) => {
   }
   return res.sendFile(path.join(__dirname, '../build', 'index.html'));
 });
-app.listen(3000, () => {
-  console.log('listened on 3000');
-});
+app.listen(443);

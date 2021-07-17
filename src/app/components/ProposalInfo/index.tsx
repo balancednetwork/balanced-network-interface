4bac579a433b399d806379d9b1dba79ef499318f
YJ <gitdiff57@gmail.com>
2021-07-17 11:13:13 +0800

    Bring styled components out of functional component scope

--- src/app/components/ProposalInfo/index.tsx
+++ src/app/components/ProposalInfo/index.tsx
@@ -2,9 +2,10 @@ import React from 'react';
 
 import dayjs from 'dayjs';
 import duration from 'dayjs/plugin/duration';
+import { Flex } from 'rebass/styled-components';
 import styled from 'styled-components';
 
-import { theme, Typography } from 'app/theme';
+import { Typography } from 'app/theme';
 import { ReactComponent as CalendarIcon } from 'assets/icons/calendar.svg';
 import { ReactComponent as PieChartIcon } from 'assets/icons/pie-chart.svg';
 import { ReactComponent as UserIcon } from 'assets/icons/users.svg';
@@ -12,52 +13,51 @@ import { normalizeContent } from 'utils';
 
 dayjs.extend(duration);
 
+const ProposalWrapper = styled.div`
+  border-radius: 10px;
+  flex: 1;
+  width: 100%;
+  background: #144a68;
+  border: 2px solid #144a68;
+  padding: 20px 25px;
+  transition: border 0.3s ease;
+  outline: none;
+  overflow: visible;
+  margin: 0 0 25px;
+  cursor: pointer;
+
+  :hover,
+  :focus {
+    border: 2px solid #2ca9b7;
+  }
+`;
+const ApprovalSwatch = styled.div`
+  margin-right: 7px;
+  background: #2ca9b7;
+  height: 20px;
+  width: 20px;
+  border-radius: 5px;
+`;
+const RejectionSwatch = styled.div`
+  margin-right: 7px;
+  background: #fb6a6a;
+  height: 20px;
+  width: 20px;
+  border-radius: 5px;
+`;
+const Divider = styled.div`
+  border-bottom: 1px solid rgba(255, 255, 255, 0.15);
+  margin-bottom: 15px;
+`;
+const ContentText = styled(Typography)`
+  color: ${({ theme }) => theme.colors.text2};
+  margin-bottom: 15px;
+  font-size: 16px;
+`;
+
 export default function ProposalInfo(props) {
-  const ProposalWrapper = styled.div`
-    border-radius: 10px;
-    flex: 1;
-    width: 100%;
-    background: #144a68;
-    border: 2px solid #144a68;
-    padding: 20px 25px;
-    transition: border 0.3s ease;
-    outline: none;
-    overflow: visible;
-    margin: 0 0 25px;
-    cursor: pointer;
-
-    :hover,
-    :focus {
-      border: 2px solid #2ca9b7;
-    }
-  `;
-  const ApprovalSwatch = styled.div`
-    margin-right: 7px;
-    background: #2ca9b7;
-    height: 20px;
-    width: 20px;
-    border-radius: 5px;
-  `;
-  const RejectionSwatch = styled.div`
-    margin-right: 7px;
-    background: #fb6a6a;
-    height: 20px;
-    width: 20px;
-    border-radius: 5px;
-  `;
-  const Divider = styled.div`
-    border-bottom: 1px solid rgba(255, 255, 255, 0.15);
-    margin-bottom: 15px;
-  `;
-  const ContentText = styled(Typography)`
-    color: ${({ theme }) => theme.colors.text2};
-    margin-bottom: 15px;
-    font-size: 16px;
-  `;
-
   const { title, content, metadata } = props;
   const { voted, voters, approvePercentage, rejectPercentage, timestamp } = metadata;
-  const themes = theme();
   return (
     <ProposalWrapper>
       <Typography variant="h3" mb="10px">
@@ -65,34 +65,30 @@ export default function ProposalInfo(props) {
       </Typography>
       <ContentText>{normalizeContent(content)}</ContentText>
       <Divider />
-      <span style={{ display: 'flex', flexDirection: 'row' }}>
+      <Flex as="span">
         <CalendarIcon height="22" width="22" style={{ marginRight: '5px' }} />
-        <Typography variant="content" color={themes.colors.white} mr="20px">
+        <Typography variant="content" color="white" mr="20px">
           {`${Math.floor(dayjs.duration(timestamp).asDays())} days, ${
             dayjs.duration(timestamp).asHours() % 24
           } hours left`}
         </Typography>
         <PieChartIcon height="22" width="22" style={{ marginRight: '5px' }} />
-        <Typography variant="content" color={themes.colors.white} mr="20px">
+        <Typography variant="content" color="white" mr="20px">
           {`${voted} voted`}
         </Typography>
         <UserIcon height="22" width="22" style={{ marginRight: '5px' }} />
-        <Typography variant="content" color={themes.colors.white} mr="20px">
+        <Typography variant="content" color="white" mr="20px">
           {`${voters} voters`}
         </Typography>
-        <span style={{ display: 'flex', flexDirection: 'row' }}>
-          <ApprovalSwatch />
-          <Typography variant="content" color={themes.colors.white} mr="20px">
-            {`${approvePercentage}%`}
-          </Typography>
-        </span>
-        <span style={{ display: 'flex', flexDirection: 'row' }}>
-          <RejectionSwatch />
-          <Typography variant="content" color={themes.colors.white} mr="20px">
-            {`${rejectPercentage}%`}
-          </Typography>
-        </span>
-      </span>
+        <ApprovalSwatch />
+        <Typography variant="content" color="white" mr="20px">
+          {`${approvePercentage}%`}
+        </Typography>
+        <RejectionSwatch />
+        <Typography variant="content" color="white" mr="20px">
+          {`${rejectPercentage}%`}
+        </Typography>
+      </Flex>
     </ProposalWrapper>
   );
 }
--- src/app/containers/VotePage/index.tsx
+++ src/app/containers/VotePage/index.tsx
@@ -9,7 +9,20 @@ import { Button } from 'app/components/Button';
 import { DefaultLayout } from 'app/components/Layout';
 import ProposalInfo from 'app/components/ProposalInfo';
 import { Typography } from 'app/theme';
-import { comparatorDesc } from 'utils';
+
+const VoteContainer = styled(Box)`
+  flex: 1;
+  border-radius: 10px;
+  padding: 35px 35px;
+  background-color: ${({ theme }) => theme.colors.bg2};
+  margin-bottom: 50px;
+`;
+
+const VoteHeader = styled(Box)`
+  display: grid;
+  grid-template-columns: repeat(auto-fit, minmax(128px, 1fr));
+  margin-bottom: 30px;
+`;
 
 const metadata = {
   voted: 16,
@@ -19,47 +32,34 @@ const metadata = {
   timestamp: 284400000,
 };
 
+const mockData = [
+  {
+    id: 3,
+    title: 'Distribute more BALN to the DAO fund by reducing the BALN for loans and liquidity pools',
+    content:
+      'Too much income is being given back to people who use Balanced. While incentivization is good, we need to prevent Balanced from becoming a platform that people use purely to earn rewards.\nTo make sure Balanced has enough income in its treasury to cover ongoing costs, like security audits, a higher bug bounty, marketing initiatives, projects that utilize Balanced’s functionality, and so on, we should redirect some of the BALN allocated to borrowers and liquidity providers to the DAO fund instead.',
+    metadata: metadata,
+    status: 'pending',
+  },
+  {
+    id: 2,
+    title: 'Proposal 2',
+    content:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas id hendrerit metus. Duis eget pellentesque ex. Pellentesque massa justo, aliquet eu dui at, pulvinar vestibulum tellus. Phasellus vel mi lobortis, iaculis libero tristique, volutpat tortor. Phasellus venenatis tellus eget tempor.\nElementum. Duis quis dapibus sapien, semper euismod dolor. Fusce at porttitor risus. Etiam vehicula massa aliquam elit sagittis pulvinar. Vivamus luctus lectus arcu, ac commodo turpis varius tempor.',
+    metadata: metadata,
+    status: 'approved',
+  },
+  {
+    id: 1,
+    title: 'Proposal 1',
+    content:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas id hendrerit metus. Duis eget pellentesque ex. Pellentesque massa justo, aliquet eu dui at, pulvinar vestibulum tellus. Phasellus vel mi lobortis, iaculis libero tristique, volutpat tortor. Phasellus venenatis tellus eget tempor.\nElementum. Duis quis dapibus sapien, semper euismod dolor. Fusce at porttitor risus. Etiam vehicula massa aliquam elit sagittis pulvinar. Vivamus luctus lectus arcu, ac commodo turpis varius tempor.',
+    metadata: metadata,
+    status: 'approved',
+  },
+];
+
 export function VotePage() {
-  const VoteContainer = styled(Box)`
-    flex: 1;
-    border-radius: 10px;
-    padding: 35px 35px;
-    background-color: ${({ theme }) => theme.colors.bg2};
-    margin-bottom: 50px;
-  `;
-
-  const VoteHeader = styled(Box)`
-    display: grid;
-    grid-template-columns: repeat(auto-fit, minmax(128px, 1fr));
-    margin-bottom: 30px;
-  `;
-
-  const mockData = [
-    {
-      id: 3,
-      title: 'Distribute more BALN to the DAO fund by reducing the BALN for loans and liquidity pools',
-      content:
-        'Too much income is being given back to people who use Balanced. While incentivization is good, we need to prevent Balanced from becoming a platform that people use purely to earn rewards.\nTo make sure Balanced has enough income in its treasury to cover ongoing costs, like security audits, a higher bug bounty, marketing initiatives, projects that utilize Balanced’s functionality, and so on, we should redirect some of the BALN allocated to borrowers and liquidity providers to the DAO fund instead.',
-      metadata: metadata,
-      status: 'pending',
-    },
-    {
-      id: 2,
-      title: 'Proposal 2',
-      content:
-        'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas id hendrerit metus. Duis eget pellentesque ex. Pellentesque massa justo, aliquet eu dui at, pulvinar vestibulum tellus. Phasellus vel mi lobortis, iaculis libero tristique, volutpat tortor. Phasellus venenatis tellus eget tempor.\nElementum. Duis quis dapibus sapien, semper euismod dolor. Fusce at porttitor risus. Etiam vehicula massa aliquam elit sagittis pulvinar. Vivamus luctus lectus arcu, ac commodo turpis varius tempor.',
-      metadata: metadata,
-      status: 'approved',
-    },
-    {
-      id: 1,
-      title: 'Proposal 1',
-      content:
-        'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas id hendrerit metus. Duis eget pellentesque ex. Pellentesque massa justo, aliquet eu dui at, pulvinar vestibulum tellus. Phasellus vel mi lobortis, iaculis libero tristique, volutpat tortor. Phasellus venenatis tellus eget tempor.\nElementum. Duis quis dapibus sapien, semper euismod dolor. Fusce at porttitor risus. Etiam vehicula massa aliquam elit sagittis pulvinar. Vivamus luctus lectus arcu, ac commodo turpis varius tempor.',
-      metadata: metadata,
-      status: 'approved',
-    },
-  ];
   return (
     <DefaultLayout title="Vote">
       <Helmet>
@@ -73,7 +73,7 @@ export function VotePage() {
           </Box>
         </VoteHeader>
         {mockData
-          .sort((a, b) => comparatorDesc(a.id, b.id))
+          .sort((a, b) => b.id - a.id)
           .map(ele => (
             <Link to={`/proposal/${ele?.id}`} style={{ textDecoration: 'none' }}>
               <ProposalInfo key={ele?.id} title={ele?.title} content={ele?.content} metadata={ele?.metadata} />

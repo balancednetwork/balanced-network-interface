b89b679ec0d675b867d5a5375285ea21ac855e40
Nga <cowslips9@gmail.com>
2021-09-08 00:30:50 +0700

    feat: create proposal config

---
+++ src/app/containers/NewProposalPage/constant.ts
@@ -0,0 +1,105 @@
+import { BalancedJs } from 'packages/BalancedJs';
+
+import bnJs from 'bnJs';
+
+const ProposalMapping = {
+  daofund: 'DAO fund',
+  baln_holders: 'BALN holders',
+  Loans: 'Borrower',
+};
+const getKeyByValue = value => {
+  return Object.keys(ProposalMapping).find(key => ProposalMapping[key] === value);
+};
+export const PROPOSAL_CONFIG = {
+  Text: undefined,
+  'BALN allocation': {
+    fetchInputData: async () =>
+      Object.entries(await bnJs.Rewards.getRecipientsSplit()).map(item => ({
+        name: ProposalMapping[item[0]] || item[0],
+        percent: BalancedJs.utils
+          .toIcx(item[1] as string)
+          .times(100)
+          .toFixed(),
+      })),
+    submitParams: ratioInputValue => {
+      const recipientList = Object.entries(ratioInputValue).map(item => ({
+        recipient_name: getKeyByValue(item[0]) || item[0],
+        dist_percent: BalancedJs.utils.toLoop(Number(item[1]) / 100).toNumber(),
+      }));
+      return {
+        updateDistPercent: { _recipient_list: recipientList },
+      };
+    },
+    validate: sum => ({ isValid: sum === 100, message: 'Allocation must equal 100%.' }),
+  },
+  'Network fee allocation': {
+    fetchInputData: async () => {
+      const res = await bnJs.Dividends.getDividendsPercentage();
+      return Object.entries(res).map(item => ({
+        name: ProposalMapping[item[0]] || item[0],
+        percent: BalancedJs.utils
+          .toIcx(item[1] as string)
+          .times(100)
+          .toFixed(),
+      }));
+    },
+    submitParams: ratioInputValue => {
+      const dist_list = Object.entries(ratioInputValue).map(item => {
+        const key = getKeyByValue(item[0]);
+        return (
+          key && {
+            [key]: BalancedJs.utils.toLoop(Number(item[1]) / 100).toNumber(),
+          }
+        );
+      });
+      return {
+        setDividendsCategoryPercentage: { _dist_list: dist_list },
+      };
+    },
+    validate: sum => ({ isValid: sum === 100, message: 'Allocation must equal 100%.' }),
+  },
+
+  'Loan fee': {
+    fetchInputData: async () => {
+      const res = await bnJs.Loans.getParameters();
+      const _percent = Number((parseInt(res['origination fee'], 16) / 100).toFixed(2));
+      return [{ percent: _percent }];
+    },
+    submitParams: ratioInputValue => {
+      const origination_fee = Number(Object.values(ratioInputValue)) * 100;
+      return { update_origination_fee: { _fee: origination_fee } };
+    },
+    validate: sum => ({
+      isValid: sum <= 10,
+      message: 'Must be less than or equal to 10%.',
+    }),
+  },
+
+  'Loan to value ratio': {
+    fetchInputData: async () => {
+      const res = await bnJs.Loans.getParameters();
+      const _percent = Number((1000000 / parseInt(res['locking ratio'], 16) / 10000).toFixed(2));
+      return [{ percent: _percent }];
+    },
+    submitParams: ratioInputValue => {
+      const locking_ratio = Math.round(1000000 / Number(Object.values(ratioInputValue)));
+      return { update_locking_ratio: { _ratio: locking_ratio } };
+    },
+    validate: sum => ({
+      isValid: sum <= 80,
+      message: 'Must be less than or equal to the liquidation threshold (80%).',
+    }),
+  },
+
+  'Rebalancing threshold': {
+    fetchInputData: async () => {
+      const res = await bnJs.Rebalancing.getPriceChangeThreshold();
+      const _percent = BalancedJs.utils.toIcx(res).times(100).toFixed();
+      return [{ percent: _percent }];
+    },
+    validate: sum => ({
+      isValid: sum <= 7.5,
+      message: 'Must be less than or equal to 7.5%.',
+    }),
+  },
+};

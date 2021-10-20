0a111afa503513526378a07d813a98be004634e9
lijac <taidashark@outlook.com>
2021-10-20 06:49:40 -0400

    refactor: remove redundant code in newly added contract classes

--- src/packages/BalancedJs/contracts/METX.ts
+++ src/packages/BalancedJs/contracts/METX.ts
@@ -1,5 +1,3 @@
-import BigNumber from 'bignumber.js';
-
 import addresses from '../addresses';
 import ContractSettings from '../contractSettings';
 import IRC2 from './IRC2';
@@ -8,28 +6,5 @@ export default class METX extends IRC2 {
   constructor(contractSettings: ContractSettings) {
     super(contractSettings);
     this.address = addresses[this.nid].metx;
-  }
-
-  balanceOf(owner: string) {
-    const callParams = this.paramsBuilder({
-      method: 'balanceOf',
-      params: {
-        _owner: owner,
-      },
-    });
-
-    return this.call(callParams);
-  }
-
-  deposit(value: BigNumber) {
-    return this.transfer(addresses[this.nid].dex, value, JSON.stringify({ method: '_deposit' }));
-  }
-
-  totalSupply() {
-    const callParams = this.paramsBuilder({
-      method: 'totalSupply',
-    });
-
-    return this.call(callParams);
   }
 }

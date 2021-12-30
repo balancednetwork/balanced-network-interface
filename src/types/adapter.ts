22758281047b7766a95e347b59e4218b4abd6289
lijac <taidashark@outlook.com>
2021-12-29 20:44:47 -0500

    refactor: add type

--- src/types/adapter.ts
+++ src/types/adapter.ts
@@ -2,6 +2,6 @@ import { SUPPORTED_TOKENS_LIST } from '../constants/tokens';
 import { Token } from './balanced-sdk-core/entities';
 import { CurrencyKey } from './index';
 
-export const getTokenFromCurrencyKey = (key?: CurrencyKey) => {
+export const getTokenFromCurrencyKey = (key?: CurrencyKey): Token | undefined => {
   if (key) return SUPPORTED_TOKENS_LIST.find((token: Token) => token.symbol === key);
 };

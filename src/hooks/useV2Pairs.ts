df17c7967bc82351def600a6fcbd251327bcac37
Wei Li <developer.clear@gmail.com>
2022-01-23 22:31:33 -0600

    fix: price update - fetch pairs info per block - refactor useV2Pairs

--- src/hooks/useV2Pairs.ts
+++ src/hooks/useV2Pairs.ts
@@ -7,6 +7,7 @@ import bnJs from 'bnJs';
 import { canBeQueue } from 'constants/currency';
 import { NULL_CONTRACT_ADDRESS } from 'constants/tokens';
 // import { useBlockNumber } from 'store/application/hooks';
+import { useBlockNumber } from 'store/application/hooks';
 import { Currency, CurrencyAmount, Token } from 'types/balanced-sdk-core';
 import { Pair } from 'types/balanced-v1-sdk';
 
@@ -22,82 +23,91 @@ export enum PairState {
 }
 
 export function useV2Pairs(currencies: [Currency | undefined, Currency | undefined][]): [PairState, Pair | null][] {
-  const [reserves, setReserves] = useState<
-    ({ reserve0: string; reserve1: string; poolId: number; totalSupply: string } | number | undefined)[]
-  >([]);
-
   const tokens = useMemo(() => {
     return currencies.map(([currencyA, currencyB]) => [currencyA?.wrapped, currencyB?.wrapped]);
   }, [currencies]);
 
-  // const lastBlockNumber = useBlockNumber();
+  const [pairs, setPairs] = useState<[PairState, Pair | null][]>(Array(tokens.length).fill([PairState.LOADING, null]));
+
+  const lastBlockNumber = useBlockNumber();
 
   useEffect(() => {
-    setReserves(Array(tokens.length).fill(PairState.LOADING));
+    setPairs(Array(tokens.length).fill([PairState.LOADING, null]));
+  }, [tokens]);
 
+  useEffect(() => {
     const fetchReserves = async () => {
       try {
         const result = await Promise.all(
-          tokens.map(async ([tokenA, tokenB]) => {
-            if (tokenA && tokenB && tokenA.chainId === tokenB.chainId && !tokenA.equals(tokenB)) {
-              let stats;
-              let poolId;
+          tokens.map(
+            async ([tokenA, tokenB]): Promise<[PairState, Pair | null]> => {
+              if (tokenA && tokenB && tokenA.chainId === tokenB.chainId && !tokenA.equals(tokenB)) {
+                let stats;
+                let poolId;
 
-              try {
-                poolId = parseInt(await bnJs.Dex.getPoolId(tokenA.address, tokenB.address), 16);
-                if (poolId === 0) return undefined;
-                stats = await bnJs.Dex.getPoolStats(poolId);
-              } catch (err) {
-                return undefined;
+                try {
+                  poolId = parseInt(await bnJs.Dex.getPoolId(tokenA.address, tokenB.address), 16);
+                  if (poolId === 0) return [PairState.NOT_EXISTS, null];
+                  stats = await bnJs.Dex.getPoolStats(poolId);
+                } catch (err) {
+                  return [PairState.NOT_EXISTS, null];
+                }
+
+                const baseReserve = new BigNumber(stats['base'], 16).toFixed();
+                const quoteReserve = new BigNumber(stats['quote'], 16).toFixed();
+                const totalSupply = new BigNumber(stats['total_supply'], 16).toFixed();
+
+                if (stats['base_token'] === tokenA.address) {
+                  return [
+                    PairState.EXISTS,
+                    new Pair(
+                      CurrencyAmount.fromRawAmount(tokenA, baseReserve),
+                      CurrencyAmount.fromRawAmount(tokenB, quoteReserve),
+                      {
+                        poolId,
+                        totalSupply,
+                      },
+                    ),
+                  ];
+                } else {
+                  return [
+                    PairState.EXISTS,
+                    new Pair(
+                      CurrencyAmount.fromRawAmount(tokenA, quoteReserve),
+                      CurrencyAmount.fromRawAmount(tokenB, baseReserve),
+                      {
+                        poolId,
+                        totalSupply,
+                      },
+                    ),
+                  ];
+                }
+              } else {
+                return [PairState.INVALID, null];
               }
-
-              const baseReserve = new BigNumber(stats['base'], 16).toFixed();
-              const quoteReserve = new BigNumber(stats['quote'], 16).toFixed();
-              const totalSupply = new BigNumber(stats['total_supply'], 16).toFixed();
-
-              if (stats['base_token'] === tokenA.address)
-                return { reserve0: baseReserve, reserve1: quoteReserve, totalSupply, poolId };
-              else return { reserve0: quoteReserve, reserve1: baseReserve, totalSupply, poolId };
-            } else return undefined;
-          }),
+            },
+          ),
         );
 
-        setReserves(result);
+        setPairs(result);
       } catch (err) {
-        setReserves(Array(tokens.length).fill(PairState.INVALID));
+        setPairs(Array(tokens.length).fill([PairState.INVALID, null]));
       }
     };
     fetchReserves();
-  }, [tokens]);
+  }, [tokens, lastBlockNumber]);
 
   const queuePair = useQueuePair();
 
   return useMemo(() => {
-    return tokens.map((tokenArr, i) => {
-      const result = reserves[i];
-      const tokenA = tokenArr[0];
-      const tokenB = tokenArr[1];
-
-      if (result === PairState.LOADING) return [PairState.LOADING, null];
-      if (!tokenA || !tokenB || tokenA.equals(tokenB)) return [PairState.INVALID, null];
-
-      if (canBeQueue(tokenA, tokenB)) return queuePair;
-
-      if (!result) return [PairState.NOT_EXISTS, null];
-
-      if (typeof result === 'number') return [PairState.INVALID, null];
-      const { reserve0, reserve1, poolId, totalSupply } = result;
-
-      const [token0, token1] = tokenA.sortsBefore(tokenB) ? [tokenA, tokenB] : [tokenB, tokenA];
-      return [
-        PairState.EXISTS,
-        new Pair(CurrencyAmount.fromRawAmount(token0, reserve0), CurrencyAmount.fromRawAmount(token1, reserve1), {
-          poolId,
-          totalSupply,
-        }),
-      ];
+    return pairs.map(pair => {
+      if (pair[0] === PairState.EXISTS && pair[1] && canBeQueue(pair[1].reserve0.currency, pair[1].reserve1.currency)) {
+        return queuePair;
+      } else {
+        return pair;
+      }
     });
-  }, [queuePair, reserves, tokens]);
+  }, [queuePair, pairs]);
 }
 
 export function useV2Pair(tokenA?: Currency, tokenB?: Currency): [PairState, Pair | null] {

a27e32504aed83658fab0a405f718d3355fac12d
Nga <cowslips9@gmail.com>
2022-02-22 22:33:38 +0700

    refactor

--- src/app/components/Banner/index.tsx
+++ src/app/components/Banner/index.tsx
@@ -5,15 +5,19 @@ import styled from 'styled-components';
 
 import { ReactComponent as CrossIcon } from 'assets/icons/cross.svg';
 
-const BannerStyled = styled(Flex)<{ close?: boolean }>`
+const BannerContainer = styled(Box)`
   ${({ theme }) => theme.mediaWidth.upMedium`
    margin: 25px 40px 0;
   `}
   ${({ theme }) => theme.mediaWidth.upLarge`
    margin: 25px auto 0;
-   max-width: 1200px;
+   max-width: 1280px;
+   padding: 0 40px;
   `}
   margin: 25px 16px 0;
+`;
+
+const BannerStyled = styled(Flex)<{ close?: boolean }>`
   padding: 15px 25px;
   border-radius: 10px;
   background: #0b284c;
@@ -34,11 +38,13 @@ export const Banner = props => {
   const [close, onClose] = React.useState(false);
 
   return (
-    <BannerStyled alignItems="center" close={close}>
-      <Box flex="1">{props.children}</Box>
-      <IconButton onClick={() => onClose(true)}>
-        <CrossIcon width="25px" height="25px" />
-      </IconButton>
-    </BannerStyled>
+    <BannerContainer>
+      <BannerStyled alignItems="center" close={close}>
+        <Box flex="1">{props.children}</Box>
+        <IconButton onClick={() => onClose(true)}>
+          <CrossIcon width="25px" height="25px" />
+        </IconButton>
+      </BannerStyled>
+    </BannerContainer>
   );
 };

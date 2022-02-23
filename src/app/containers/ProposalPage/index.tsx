d56598120f5b92b14e3d8b4ac238b27a5c9ebe9b
hetfly <hetfly@gmail.com>
2022-02-23 19:43:30 +0100

    fix: hotfix proposal active voting

--- src/app/containers/ProposalPage/index.tsx
+++ src/app/containers/ProposalPage/index.tsx
@@ -118,12 +118,15 @@ export function ProposalPage() {
 
   const proposalType = actionKeyList.map(actionKey => getKeyByValue(actionKey)).filter(item => item)[0];
 
-  const isActive =
-    proposal &&
-    platformDay &&
-    proposal.status === 'Active' &&
-    proposal.startDay <= platformDay &&
-    proposal.endDay > platformDay;
+  // const isActive =
+  //   proposal &&
+  //   platformDay &&
+  //   proposal.status === 'Active' &&
+  //   proposal.startDay <= platformDay &&
+  //   proposal.endDay > platformDay;
+
+  const isActive = proposal && platformDay && proposal.status === 'Active';
+
   const hasUserVoted = isActive && userStatus?.hasVoted;
 
   const { account } = useIconReact();

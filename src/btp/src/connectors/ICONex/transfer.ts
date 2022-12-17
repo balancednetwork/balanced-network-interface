0467ee9108cf67411ed6c7b43a3c2402f9410eed
NgaNTT <cowslips9@gmail.com>
2022-12-18 00:10:14 +0700

    refactor: change js to ts

--- src/btp/src/connectors/ICONex/transfer.ts
+++ src/btp/src/connectors/ICONex/transfer.ts
@@ -1,16 +1,16 @@
-import { tokenTypes } from 'btp/src/utils/constants';
+import { TransferTransactionRequest } from 'btp/src/type/transaction';
 
-import { checkIsToken } from '../chainConfigs';
+import { checkIRC2Token } from '../chainConfigs';
 import { SIGNING_ACTIONS } from '../constants';
 import { sendNativeCoin, setApproveForSendNonNativeCoin, approveIRC2 } from './ICONServices';
 
-export const transfer = (tx, isSendingNativeCoin, token) => {
+export const transfer = (tx: TransferTransactionRequest, isSendingNativeCoin: boolean, token: string) => {
   window[SIGNING_ACTIONS.GLOBAL_NAME] = SIGNING_ACTIONS.TRANSFER;
-  const isToken = checkIsToken(token);
+  const isIRC2Token = checkIRC2Token(token);
 
   if (isSendingNativeCoin) {
     return sendNativeCoin(tx);
-  } else if (isToken && isToken.type === tokenTypes.IRC2) {
+  } else if (isIRC2Token) {
     return approveIRC2(tx);
   } else {
     return setApproveForSendNonNativeCoin(tx);

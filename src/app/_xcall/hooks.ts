5df2ba97c250192b2f0edbff92b87166053dae6a
hetfly <hetfly@gmail.com>
2023-12-12 14:56:54 +0100

    fix: check auto execute status when checking user available gas

--- src/app/_xcall/hooks.ts
+++ src/app/_xcall/hooks.ts
@@ -4,6 +4,8 @@ import { useQuery, UseQueryResult } from 'react-query';
 import { useICX } from 'constants/tokens';
 import { useCrossChainWalletBalances, useSignedInWallets } from 'store/wallet/hooks';
 
+import { AUTO_EXECUTION_ON_ICON } from './_icon/config';
+import { AUTO_EXECUTION_ON_ARCHWAY } from './archway/config';
 import { useARCH } from './archway/tokens';
 import { SupportedXCallChains } from './types';
 
@@ -28,9 +30,10 @@ export function useXCallGasChecker(
 
       if (chain1 === 'archway' || chain2 === 'archway') {
         const gasAmount = balances['archway'] && balances['archway'][ARCH.address];
-        const hasEnoughGas = gasAmount
-          ? !CurrencyAmount.fromRawAmount(ARCH, ARCHWAY_GAS_THRESHOLD * 10 ** ARCH.decimals).greaterThan(gasAmount)
-          : false;
+        const hasEnoughGas =
+          AUTO_EXECUTION_ON_ARCHWAY || gasAmount
+            ? !CurrencyAmount.fromRawAmount(ARCH, ARCHWAY_GAS_THRESHOLD * 10 ** ARCH.decimals).greaterThan(gasAmount)
+            : false;
         if (!hasEnoughGas) {
           errorMessage = `You need at least ${ARCHWAY_GAS_THRESHOLD} ARCH to pay for transaction fees on Archway.`;
         }
@@ -39,9 +42,10 @@ export function useXCallGasChecker(
 
       if (icon || chain1 === 'icon' || chain2 === 'icon') {
         const gasAmount = balances['icon'] && balances['icon'][ICX.address];
-        const hasEnoughGas = gasAmount
-          ? !CurrencyAmount.fromRawAmount(ICX, ICX_GAS_THRESHOLD * 10 ** ICX.decimals).greaterThan(gasAmount)
-          : false;
+        const hasEnoughGas =
+          AUTO_EXECUTION_ON_ICON || gasAmount
+            ? !CurrencyAmount.fromRawAmount(ICX, ICX_GAS_THRESHOLD * 10 ** ICX.decimals).greaterThan(gasAmount)
+            : false;
         if (!hasEnoughGas) {
           errorMessage = `You need at least ${ICX_GAS_THRESHOLD} ICX to pay for transaction fees on ICON.`;
         }

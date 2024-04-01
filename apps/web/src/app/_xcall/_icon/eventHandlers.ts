493991dbaaa92e91ff055153940a7af6e13b20a3
0xmilktea <0xmilktea@gmail.com>
2024-04-02 01:11:32 +0800

    fix icon event handler

--- apps/web/src/app/_xcall/_icon/eventHandlers.ts
+++ apps/web/src/app/_xcall/_icon/eventHandlers.ts
@@ -40,8 +40,6 @@ export const useICONEventListener = () => {
   const listeningTo = useXCallListeningTo();
   const setListeningTo = useSetListeningTo();
   const eventName: XCallEventType | null = listeningTo?.chain === 'icon' ? listeningTo.event : null;
-  const [socket, setSocket] = React.useState<WebSocket | undefined>(undefined);
-  const event = eventName && `${getICONEventSignature(eventName)}`;
   const iconBlockHeight = useBlockNumber();
   const blockHeightRef = React.useRef<string | null>(null);
   const addDestinationEvent = useAddDestinationEvent();
@@ -54,7 +52,7 @@ export const useICONEventListener = () => {
   const addOriginEvent = useAddOriginEvent();
 
   React.useEffect(() => {
-    if (event && iconBlockHeight) {
+    if (iconBlockHeight) {
       if (!blockHeightRef.current) {
         blockHeightRef.current = `0x${iconBlockHeight.toString(16)}`;
       }
@@ -63,143 +61,137 @@ export const useICONEventListener = () => {
     }
   }, [iconBlockHeight, event]);
 
-  const query = React.useRef({ ...QUERY_PARAMS });
-  if (event && blockHeightRef.current) {
-    query.current.eventFilters[0].event = event;
-    query.current.height = blockHeightRef.current;
-  }
-
-  const disconnectFromWebsocket = React.useCallback(() => {
-    if (!socket || socket.readyState !== WebSocket.OPEN) return;
-    socket.close();
-    setSocket(undefined);
-  }, [socket]);
-
   React.useEffect(() => {
     if (eventName) {
-      if (!socket) {
-        const websocket = new WebSocket(ICON_WEBSOCKET_URL);
+      const websocket = new WebSocket(ICON_WEBSOCKET_URL);
 
-        websocket.onopen = () => {
-          websocket.send(JSON.stringify(query.current));
-        };
+      const eventSignature = getICONEventSignature(eventName);
+      const query = {
+        height: blockHeightRef.current,
+        eventFilters: [
+          {
+            event: eventSignature,
+            addr: bnJs.XCall.address,
+          },
+        ],
+        logs: '0x1',
+      };
 
-        websocket.onmessage = async event => {
-          const eventData = JSON.parse(event.data);
+      websocket.onopen = () => {
+        websocket.send(JSON.stringify(query));
+      };
 
-          if (eventData) {
-            switch (eventName) {
-              case XCallEvent.CallMessage: {
-                if (eventData.logs) {
-                  const callMessageLog =
-                    eventData.logs &&
-                    eventData.logs[0][0].find(log => log.indexed[0] === getICONEventSignature(XCallEvent.CallMessage));
-                  if (callMessageLog) {
-                    const snRaw = callMessageLog.indexed[3];
-                    const sn = snRaw && parseInt(snRaw, 16);
-                    const reqId = parseInt(callMessageLog.data[0], 16);
-                    const data = callMessageLog.data[1];
-                    //todo: handle parsing from above like the line below
-                    // const destinationEventData = getDestinationEventDataFromICONEvent(callMessageLog);
-                    if (sn && reqId) {
-                      //todo find origin event from arbitrary chain - not archway only
-                      const originEvent = archwayOriginEvents.find(e => e.sn === sn);
-                      if (originEvent) {
-                        addDestinationEvent('icon', {
-                          sn,
-                          reqId,
-                          data,
-                          eventName: XCallEvent.CallMessage,
-                          chain: 'icon',
-                          origin: 'archway',
-                          autoExecute: originEvent.autoExecute,
-                        });
-                        disconnectFromWebsocket();
+      websocket.onmessage = async event => {
+        const eventData = JSON.parse(event.data);
 
-                        if (originEvent.autoExecute) {
-                          setListeningTo('icon', XCallEvent.CallExecuted);
-                        }
+        if (eventData) {
+          switch (eventName) {
+            case XCallEvent.CallMessage: {
+              if (eventData.logs) {
+                const callMessageLog =
+                  eventData.logs &&
+                  eventData.logs[0][0].find(log => log.indexed[0] === getICONEventSignature(XCallEvent.CallMessage));
+                if (callMessageLog) {
+                  const snRaw = callMessageLog.indexed[3];
+                  const sn = snRaw && parseInt(snRaw, 16);
+                  const reqId = parseInt(callMessageLog.data[0], 16);
+                  const data = callMessageLog.data[1];
+                  //todo: handle parsing from above like the line below
+                  // const destinationEventData = getDestinationEventDataFromICONEvent(callMessageLog);
+                  if (sn && reqId) {
+                    //todo find origin event from arbitrary chain - not archway only
+                    const originEvent = archwayOriginEvents.find(e => e.sn === sn);
+                    if (originEvent) {
+                      addDestinationEvent('icon', {
+                        sn,
+                        reqId,
+                        data,
+                        eventName: XCallEvent.CallMessage,
+                        chain: 'icon',
+                        origin: 'archway',
+                        autoExecute: originEvent.autoExecute,
+                      });
+
+                      if (originEvent.autoExecute) {
+                        setListeningTo('icon', XCallEvent.CallExecuted);
                       }
                     }
                   }
                 }
-                break;
               }
-              case XCallEvent.CallExecuted: {
-                if (eventData.logs) {
-                  const blockHeight = eventData.height;
-                  const indexes = eventData.indexes;
-                  const executeCallLog =
-                    eventData.logs &&
-                    eventData.logs[0][0].find(log => log.indexed[0] === getICONEventSignature(XCallEvent.CallExecuted));
-                  if (executeCallLog) {
-                    const reqIdRaw = executeCallLog.indexed[1];
-                    const reqId = parseInt(executeCallLog.indexed[1], 16);
-                    const success = executeCallLog.data[0] === '0x1';
+              break;
+            }
+            case XCallEvent.CallExecuted: {
+              if (eventData.logs) {
+                const blockHeight = eventData.height;
+                const indexes = eventData.indexes;
+                const executeCallLog =
+                  eventData.logs &&
+                  eventData.logs[0][0].find(log => log.indexed[0] === getICONEventSignature(XCallEvent.CallExecuted));
+                if (executeCallLog) {
+                  const reqIdRaw = executeCallLog.indexed[1];
+                  const reqId = parseInt(executeCallLog.indexed[1], 16);
+                  const success = executeCallLog.data[0] === '0x1';
 
-                    if (reqId) {
-                      const destinationEvent = iconDestinationEvents.find(e => e.reqId === reqId);
-                      const tx = await getTxFromCallExecutedLog(blockHeight, indexes, reqIdRaw);
-                      if (tx) {
-                        disconnectFromWebsocket();
-                        if (success) {
-                          //check for callMessageSent event
-                          const callMessageSentLog = getCallMessageSentEventFromLogs(tx.eventLogs);
-                          if (callMessageSentLog) {
-                            //todo: find the destination event and determine destination for this new origin event
-                            //todo: fill description from the destination event as well
-                            const originEventData = getXCallOriginEventDataFromICON(
-                              callMessageSentLog,
-                              'archway',
-                              'Swap',
-                              '',
-                            );
+                  if (reqId) {
+                    const destinationEvent = iconDestinationEvents.find(e => e.reqId === reqId);
+                    const tx = await getTxFromCallExecutedLog(blockHeight, indexes, reqIdRaw);
+                    if (tx) {
+                      if (success) {
+                        //check for callMessageSent event
+                        const callMessageSentLog = getCallMessageSentEventFromLogs(tx.eventLogs);
+                        if (callMessageSentLog) {
+                          //todo: find the destination event and determine destination for this new origin event
+                          //todo: fill description from the destination event as well
+                          const originEventData = getXCallOriginEventDataFromICON(
+                            callMessageSentLog,
+                            'archway',
+                            'Swap',
+                            '',
+                          );
 
-                            destinationEvent && removeEvent(destinationEvent.sn, false);
-                            //add new origin event
-                            originEventData && addOriginEvent('icon', originEventData);
-                          } else {
-                            destinationEvent && removeEvent(destinationEvent.sn, true);
-                            stopListening();
-                          }
+                          destinationEvent && removeEvent(destinationEvent.sn, false);
+                          //add new origin event
+                          originEventData && addOriginEvent('icon', originEventData);
                         } else {
-                          if (destinationEvent) {
-                            //todo: not tested yet
-                            rollBackFromOrigin(destinationEvent.origin, destinationEvent.sn);
-                            setListeningTo(destinationEvent.origin, XCallEvent.RollbackMessage);
-                          }
+                          destinationEvent && removeEvent(destinationEvent.sn, true);
+                          stopListening();
+                        }
+                      } else {
+                        if (destinationEvent) {
+                          //todo: not tested yet
+                          rollBackFromOrigin(destinationEvent.origin, destinationEvent.sn);
+                          setListeningTo(destinationEvent.origin, XCallEvent.RollbackMessage);
                         }
                       }
                     }
                   }
                 }
-                break;
               }
-              case XCallEvent.ResponseMessage: {
-                break;
-              }
-              case XCallEvent.RollbackMessage: {
-                break;
-              }
+              break;
+            }
+            case XCallEvent.ResponseMessage: {
+              break;
+            }
+            case XCallEvent.RollbackMessage: {
+              break;
             }
           }
-        };
+        }
+      };
 
-        websocket.onerror = error => {
-          console.error(error);
-          disconnectFromWebsocket();
-        };
-        websocket.onclose = () => {};
-        setSocket(websocket);
-      }
-    } else {
-      disconnectFromWebsocket();
+      websocket.onerror = error => {
+        console.error(error);
+      };
+
+      websocket.onclose = () => {};
+
+      return () => {
+        websocket.close();
+      };
     }
   }, [
-    socket,
-    disconnectFromWebsocket,
     eventName,
-    query,
     addDestinationEvent,
     archwayOriginEvents,
     iconDestinationEvents,

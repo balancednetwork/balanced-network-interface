b660f88b547319b343b0addb309ec59a799d1f68
swiftcc624 <swiftcc624>
2024-04-10 20:19:10 +1000

    reorganize routes

--- apps/web/src/app/Routes.tsx
+++ apps/web/src/app/Routes.tsx
@@ -1,9 +1,9 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 
 import { MessageDescriptor } from '@lingui/core';
 import { defineMessage } from '@lingui/macro';
 import { Helmet } from 'react-helmet-async';
-import { Routes, Route, useLocation } from 'react-router-dom';
+import { Routes, Route, useLocation, Outlet } from 'react-router-dom';
 
 import { DefaultLayout } from 'app/components/Layout';
 
@@ -14,49 +14,65 @@ import { TradePage } from './containers/TradePage/Loadable';
 import { VotePage } from './containers/VotePage/Loadable';
 import { ProposalList } from './containers/VotePage/ProposalsPanel/ProposalList';
 
+import { ClaimGoodwill } from './containers/Claim/Goodwill';
+import { Claim } from './containers/Claim/LegacyFees';
+
 const routeTexts: [string, MessageDescriptor][] = [
   ['/vote', defineMessage({ message: 'Vote' })],
   ['/trade', defineMessage({ message: 'Trade' })],
   ['/', defineMessage({ message: 'Home' })],
 ];
 
+const Redirect = ({ to }) => {
+  useEffect(() => {
+    window.location.href = to;
+    throw new Error('Redirecting...');
+  }, [to]);
+  return null;
+};
+
 export default function RootRoutes() {
   const location = useLocation();
-
   const title = routeTexts.find(item => location.pathname.startsWith(item[0]))?.[1];
 
   return (
-    <DefaultLayout title={title?.id}>
-      <Helmet>
-        <title>{title?.message}</title>
-      </Helmet>
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/vote" element={<VotePage />} />
-        <Route path="/vote/proposal-list" element={<ProposalList />} />
-        <Route path="/trade" element={<TradePage />} />
-        <Route path="/trade/:pair" element={<TradePage />} />
-        <Route path="/trade/supply" element={<TradePage />} />
-        <Route path="/trade/supply/:pair" element={<TradePage />} />
-        <Route path="/trade/bridge/" element={<TradePage />} />
-        <Route path="/vote/new-proposal" element={<NewProposalPage />} />
-        <Route path="/vote/proposal/:id" element={<ProposalPage />} />
-        <Route
-          path="/airdrip"
-          element={<>Empty</>}
-          // element={() => {
-          //   window.location.href = 'https://balanced.network/';
-          //   return null;
-          // }}
-        />
-        <Route
-          element={<>Empty</>}
-          // element={() => {
-          //   window.location.href = 'https://balanced.network/404';
-          //   return null;
-          // }}
-        />
-      </Routes>
-    </DefaultLayout>
+    <Routes>
+      <Route
+        path="*"
+        element={
+          <>
+            <DefaultLayout title={title?.id}>
+              <Helmet>
+                <title>{title?.message}</title>
+              </Helmet>
+              <Outlet />
+            </DefaultLayout>
+          </>
+        }
+      >
+        <Route index element={<HomePage />} />
+
+        <Route path="trade" element={<Outlet />}>
+          <Route index element={<TradePage />} />
+          <Route path=":pair" element={<TradePage />} />
+          <Route path="supply" element={<TradePage />} />
+          <Route path="supply/:pair" element={<TradePage />} />
+          <Route path="bridge/" element={<TradePage />} />
+        </Route>
+
+        <Route path="vote" element={<Outlet />}>
+          <Route index element={<VotePage />} />
+          <Route path="proposal-list" element={<ProposalList />} />
+          <Route path="new-proposal" element={<NewProposalPage />} />
+          <Route path="proposal/:id" element={<ProposalPage />} />
+        </Route>
+
+        <Route path="airdrip" element={<Redirect to="https://balanced.network/" />} />
+        <Route path="*" element={<Redirect to="https://balanced.network/404" />} />
+      </Route>
+
+      <Route path="/claim-legacy-fees" element={<Claim />} />
+      <Route path="/claim-goodwill" element={<ClaimGoodwill />} />
+    </Routes>
   );
 }
--- apps/web/src/app/index.tsx
+++ apps/web/src/app/index.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 
 import { Helmet } from 'react-helmet-async';
 import { useTranslation } from 'react-i18next';
-import { Routes, Route } from 'react-router-dom';
 
 import NotificationContainer from 'app/components/Notification/NotificationContainer';
 import WalletModal from 'app/components/WalletModal';
@@ -13,8 +12,6 @@ import TransactionUpdater from 'store/transactions/updater';
 import { useICONEventListener } from './_xcall/_icon/eventHandlers';
 import { useArchwayEventListener } from './_xcall/archway/eventHandler';
 import TransferAssetsModal from './components/BTP/index';
-import { ClaimGoodwill } from './containers/Claim/Goodwill';
-import { Claim } from './containers/Claim/LegacyFees';
 import RootRoutes from './Routes';
 
 function Updaters() {
@@ -44,11 +41,7 @@ export function App() {
         {/* Add message for community */}
 
         <Helmet titleTemplate="%s | Balanced" defaultTitle="Balanced" htmlAttributes={{ lang: i18n.language }} />
-        <Routes>
-          <Route path="/claim-legacy-fees" element={<Claim />} />
-          <Route path="/claim-goodwill" element={<ClaimGoodwill />} />
-          <Route path="*" element={<RootRoutes />} />
-        </Routes>
+        <RootRoutes />
       </ThemeProvider>
     </>
   );

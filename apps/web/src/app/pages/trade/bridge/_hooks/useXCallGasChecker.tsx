8de44dd8824b6d41ed68a893db8a66a260bbf125
swiftcc624 <swiftcc624@gmail.com>
2024-05-25 22:24:37 +1000

    fix useXCallGasChecker for icon

--- apps/web/src/app/pages/trade/bridge/_hooks/useXCallGasChecker.tsx
+++ apps/web/src/app/pages/trade/bridge/_hooks/useXCallGasChecker.tsx
@@ -3,29 +3,52 @@ import { XChain, XChainId } from '../types';
 import { xChainMap } from '../_config/xChains';
 import { useMemo } from 'react';
 import { getNetworkDisplayName } from '../utils';
+import { useICX } from 'constants/tokens';
 
 function useXCallGasChecker(xChainId: XChainId): { hasEnoughGas: boolean; errorMessage: string } {
   const balances = useCrossChainWalletBalances();
+  const ICX = useICX();
 
   const { hasEnoughGas, errorMessage } = useMemo(() => {
     try {
-      const xChain: XChain = xChainMap[xChainId];
-      const nativeCurrency = xChain?.nativeCurrency;
+      if (xChainId === '0x1.icon') {
+        const xChain: XChain = xChainMap[xChainId];
+        const nativeCurrency = xChain?.nativeCurrency;
 
-      const hasEnoughGas =
-        balances[xChainId] &&
-        balances[xChainId]?.['native'].greaterThan(xChain.gasThreshold * 10 ** nativeCurrency.decimals);
-      const errorMessage = !hasEnoughGas
-        ? `You need at least ${xChain.gasThreshold} ${
-            nativeCurrency.symbol
-          } in your wallet to pay for transaction fees on ${getNetworkDisplayName(xChainId)}.`
-        : '';
+        // TODO: balances[xChainId]?.['native'] is undefined, used ICX.address instead
+        const hasEnoughGas =
+          balances[xChainId] &&
+          balances[xChainId]?.[ICX.address].greaterThan(xChain.gasThreshold * 10 ** nativeCurrency.decimals);
 
-      return { hasEnoughGas: !!hasEnoughGas, errorMessage };
-    } catch (e) {}
+        const errorMessage = !hasEnoughGas
+          ? `You need at least ${xChain.gasThreshold} ${
+              nativeCurrency.symbol
+            } in your wallet to pay for transaction fees on ${getNetworkDisplayName(xChainId)}.`
+          : '';
 
-    return { hasEnoughGas: false, errorMessage: '' };
-  }, [balances, xChainId]);
+        return { hasEnoughGas: !!hasEnoughGas, errorMessage };
+      } else {
+        const xChain: XChain = xChainMap[xChainId];
+        const nativeCurrency = xChain?.nativeCurrency;
+
+        const hasEnoughGas =
+          balances[xChainId] &&
+          balances[xChainId]?.['native'].greaterThan(xChain.gasThreshold * 10 ** nativeCurrency.decimals);
+
+        const errorMessage = !hasEnoughGas
+          ? `You need at least ${xChain.gasThreshold} ${
+              nativeCurrency.symbol
+            } in your wallet to pay for transaction fees on ${getNetworkDisplayName(xChainId)}.`
+          : '';
+
+        return { hasEnoughGas: !!hasEnoughGas, errorMessage };
+      }
+    } catch (e) {
+      // console.log(e);
+    }
+
+    return { hasEnoughGas: false, errorMessage: 'Unknown' };
+  }, [balances, xChainId, ICX]);
 
   return {
     hasEnoughGas,

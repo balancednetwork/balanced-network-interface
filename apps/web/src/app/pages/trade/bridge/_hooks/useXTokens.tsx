d203b147f5cb10618c63c3d04bdadddb6e00f199
0xmilktea <0xmilktea@gmail.com>
2024-05-27 10:04:46 +0800

    fix: remove duplicated items in wallet UI

--- apps/web/src/app/pages/trade/bridge/_hooks/useXTokens.tsx
+++ apps/web/src/app/pages/trade/bridge/_hooks/useXTokens.tsx
@@ -1,11 +1,21 @@
-import { XChainId } from '../types';
+import { XChainId, XToken } from '../types';
 import { xTokenMap } from '../_config/xTokens';
 
 const useXTokens = (from: XChainId, to?: XChainId) => {
   if (to) {
     return xTokenMap[from]?.[to];
   } else {
-    return Object.values(xTokenMap[from] || {}).flat();
+    return (
+      Object.values(xTokenMap[from] || {})
+        .flat()
+        // to remove duplicated items
+        .reduce((a: XToken[], c: XToken) => {
+          if (!a.some(v => v.address === c.address)) {
+            a.push(c);
+          }
+          return a;
+        }, [])
+    );
   }
 };
 

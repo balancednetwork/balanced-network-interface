d629aeeeb46800afaa24ccf92c851c2e810d41f9
0xmilktea <0xmilktea@gmail.com>
2024-07-25 15:34:09 +0800

    fix

--- apps/web/src/app/pages/trade/bridge/_hooks/useEVMReact.tsx
+++ apps/web/src/app/pages/trade/bridge/_hooks/useEVMReact.tsx
@@ -1,11 +1,14 @@
 import { useAccount, useDisconnect } from 'wagmi';
 import { useMemo } from 'react';
 import { xChains } from '../_config/xChains';
+import { XWalletType } from '../types';
 
 const useEVMReact = () => {
   const { address, chainId } = useAccount();
   const { disconnectAsync } = useDisconnect();
-  const xChainId = xChains.find(xChain => xChain.id === chainId)?.xChainId;
+  const xChainId = xChains
+    .filter(c => c.xWalletType === XWalletType.EVM)
+    .find(xChain => xChain.id === chainId)?.xChainId;
 
   return useMemo(
     () => ({

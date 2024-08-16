40a4133aa47f0667ac5497f52c91235f32c53d76
swiftcc624 <swiftcc624@gmail.com>
2024-08-16 22:31:19 +0800

    remove

--- apps/web/src/app/pages/trade/supply/_components/LiquidityDetails/StakeLPPanel.tsx
+++
@@ -1,7 +0,0 @@
-import React, { useEffect, useCallback, useMemo } from 'react';
-
-import { Pair } from '@balancednetwork/v1-sdk';
-
-export default function StakeLPPanel({ pair }: { pair: Pair }) {
-  return <></>;
-}

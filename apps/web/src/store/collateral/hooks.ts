fee6651bce0597140bca7d38b6bc44679196e9da
swiftcc624 <swiftcc624@gmail.com>
2024-08-16 23:44:57 +0800

    remove

--- apps/web/src/store/collateral/hooks.ts
+++ apps/web/src/store/collateral/hooks.ts
@@ -2,31 +2,13 @@ import React from 'react';
 
 import { CallData, addresses } from '@balancednetwork/balanced-js';
 import { UseQueryResult, useQuery } from '@tanstack/react-query';
-import { useDispatch, useSelector } from 'react-redux';
+import { useSelector } from 'react-redux';
 
 import bnJs from '@/bnJs';
 import { NETWORK_ID } from '@/constants/config';
-import { IcxDisplayType } from '@/types';
 import { formatUnits } from '@/utils';
 
 import { AppState } from '../index';
-import { changeIcxDisplayType } from './reducer';
-
-export const DEFAULT_COLLATERAL_TOKEN = 'sICX';
-
-export function useCollateralChangeIcxDisplayType(): (icxDisplayType: IcxDisplayType) => void {
-  const dispatch = useDispatch();
-
-  return React.useCallback(
-    (icxDisplayType: IcxDisplayType) => {
-      dispatch(changeIcxDisplayType({ icxDisplayType }));
-    },
-    [dispatch],
-  );
-}
-export function useAllDepositedAmounts() {
-  return useSelector((state: AppState) => state.collateral.depositedAmounts);
-}
 
 export function useCollateralType() {
   return useSelector((state: AppState) => state.collateral.collateralType);

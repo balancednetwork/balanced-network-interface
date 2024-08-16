eda5681d7490ff2f28d136497d80357574423d46
swiftcc624 <swiftcc624@gmail.com>
2024-08-16 23:14:00 +0800

    remove

--- apps/web/src/store/liveVoting/types.ts
+++ apps/web/src/store/liveVoting/types.ts
@@ -1,22 +1,4 @@
 import { Fraction } from '@balancednetwork/sdk-core';
-import BigNumber from 'bignumber.js';
-
-export type VoteSourceRaw = {
-  type: string;
-  votable: string;
-  weight: string;
-  currentWeight: string;
-  currentBias: string;
-  currentSlope: string;
-};
-
-export type VoteSource = {
-  type: number;
-  weight: Fraction;
-  currentWeight: Fraction;
-  currentBias: BigNumber;
-  currentSlope: BigNumber;
-};
 
 export type RewardDistributionRaw = {
   Base: Map<string, Fraction>;
@@ -28,18 +10,4 @@ export type RewardDistribution = {
   Base: Map<string, Fraction>;
   Fixed: Map<string, Fraction>;
   Voting: Map<string, Fraction>;
-};
-
-export type VoteItemInfoRaw = {
-  end: string;
-  lastVote: string;
-  power: string;
-  slope: string;
-};
-
-export type VoteItemInfo = {
-  end: Date;
-  lastVote: Date;
-  power: Fraction;
-  slope: BigNumber;
 };

45798eac20ed1ebc09353401cab2166d385fa022
swiftcc624 <swiftcc624@gmail.com>
2024-08-16 21:30:32 +0800

    remove stats app

--- apps/stats/src/bnJs.ts
+++
@@ -1,5 +0,0 @@
-import { BalancedJs } from '@balancednetwork/balanced-js';
-
-const bnJs = new BalancedJs({ networkId: parseInt(process.env.REACT_APP_NETWORK_ID ?? '1') });
-
-export default bnJs;
--- apps/stats/src/queries/historicalData/utils.ts
+++
@@ -1,8 +0,0 @@
-import { Granularity } from './types';
-
-export const GRANULARITY_MILLISECONDS: { [key in Granularity]: number } = {
-  daily: 86400000,
-  weekly: 604800000,
-  biweekly: 604800000 * 2,
-  monthly: 2628000000,
-};

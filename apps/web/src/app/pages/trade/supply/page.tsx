5ac6fb24cf5a7daabd919bf0465d2db2005f07db
swiftcc624 <swiftcc624@gmail.com>
2024-08-16 21:48:47 +0800

    empty supply page

--- apps/web/src/app/pages/trade/supply/page.tsx
+++ apps/web/src/app/pages/trade/supply/page.tsx
@@ -1,41 +1,5 @@
-import React, { useMemo } from 'react';
-
-import { useIconReact } from '@/packages/icon-react';
-
-import LPPanel from './_components/LPPanel';
-import { PoolPanelContext } from './_components/PoolPanelContext';
-import LiquidityPoolsPanel from './_components/LiquidityPoolsPanel';
-
-import { useTrackedTokenPairs } from '@/store/user/hooks';
-import { useAvailablePairs, useBalances } from '@/hooks/useV2Pairs';
+import React from 'react';
 
 export function SupplyPage() {
-  const { account } = useIconReact();
-
-  const trackedTokenPairs = useTrackedTokenPairs();
-
-  // fetch the reserves for all V2 pools
-  const pairs = useAvailablePairs(trackedTokenPairs);
-
-  // fetch the user's balances of all tracked V2 LP tokens
-  const balances = useBalances(account, pairs);
-
-  const data = useMemo(
-    () => ({
-      trackedTokenPairs,
-      pairs,
-      balances,
-    }),
-    [trackedTokenPairs, pairs, balances],
-  );
-
-  return (
-    <PoolPanelContext.Provider value={data}>
-      <LPPanel />
-
-      <div style={{ marginTop: '50px' }}>
-        <LiquidityPoolsPanel />
-      </div>
-    </PoolPanelContext.Provider>
-  );
+  return <></>;
 }

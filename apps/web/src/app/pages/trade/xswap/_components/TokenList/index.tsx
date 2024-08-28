5564122c04ebb53fbb460d54eec591090b68e10d
hetfly <hetfly@gmail.com>
2024-08-28 10:03:49 +0200

    feat: change default sort to price change

--- apps/web/src/app/pages/trade/xswap/_components/TokenList/index.tsx
+++ apps/web/src/app/pages/trade/xswap/_components/TokenList/index.tsx
@@ -55,7 +55,7 @@ export const Grid = styled.div`
 
 const TokenList = () => {
   const { data: allTokens } = useAllTokensByAddress();
-  const { sortBy, handleSortSelect, sortData } = useSort({ key: 'name', order: 'ASC' });
+  const { sortBy, handleSortSelect, sortData } = useSort({ key: 'price_24h_change', order: 'DESC' });
   const [showingExpanded, setShowingExpanded] = React.useState(false);
   const theme = useTheme();
   const isSmallScreen = useMedia(`(minWidth: ${theme.mediaWidth.upSmall})`);

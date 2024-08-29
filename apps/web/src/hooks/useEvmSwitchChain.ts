f971335eac3fc77c1e2bd3e2f14cfa2379a7bc88
swiftcc624 <swiftcc624@gmail.com>
2024-08-29 17:31:26 +0800

    disable switching to injective temporarily for debugging

--- apps/web/src/hooks/useEvmSwitchChain.ts
+++ apps/web/src/hooks/useEvmSwitchChain.ts
@@ -22,6 +22,7 @@ export const useEvmSwitchChain = (expectedXChainId: XChainId) => {
     return (
       (xChainType === 'EVM' && chainId !== expectedChainId) ||
       (xChainType === 'INJECTIVE' &&
+        false &&
         injectiveXService.walletStrategy.getWallet() === Wallet.Metamask &&
         ethereumChainId !== mainnet.id)
     );

0f3acd65f60a5864a010b67cbbc59713c62b69b3
swiftcc624 <56027533+swiftcc624@users.noreply.github.com>
2024-09-03 00:43:46 +1000

    fix 154 - gas checker unknown error (#1435)

--- apps/web/src/hooks/useXCallGasChecker.tsx
+++ apps/web/src/hooks/useXCallGasChecker.tsx
@@ -22,9 +22,7 @@ function useXCallGasChecker(xChainId: XChainId): { hasEnoughGas: boolean; errorM
               Math.round(xChain.gasThreshold * 10 ** nativeCurrency.decimals),
             )
           : balances[xChainId] &&
-            balances[xChainId]?.[nativeCurrency.symbol].greaterThan(
-              Math.round(xChain.gasThreshold * 10 ** nativeCurrency.decimals),
-            );
+            balances[xChainId]?.['native'].greaterThan(Math.round(xChain.gasThreshold * 10 ** nativeCurrency.decimals));
 
       const errorMessage = !hasEnoughGas
         ? `You need at least ${formatBigNumber(new BigNumber(xChain.gasThreshold), 'currency')} ${

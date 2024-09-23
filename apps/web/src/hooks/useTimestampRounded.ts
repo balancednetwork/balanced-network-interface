82e550216a4178cba022ad969ac8ead7d6be122b
swiftcc624 <swiftcc624@gmail.com>
2024-09-24 02:06:20 +0800

    remove unused files

--- apps/web/src/hooks/useTimestampRounded.ts
+++
@@ -1,4 +0,0 @@
-export default function useTimestampRounded(period = 1000 * 60, daysBack = 0) {
-  const now = Math.floor(new Date().getTime() / period) * period;
-  return now - daysBack * 24 * 60 * 60 * 1000;
-}

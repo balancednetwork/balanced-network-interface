c42f75aaa12f0554154bf9726a8edeef266a0518
swiftcc624 <swiftcc624@gmail.com>
2024-09-24 02:03:08 +0800

    remove SwapDescription and TokenList from swap page

--- apps/web/src/app/pages/trade/xswap/page.tsx
+++ apps/web/src/app/pages/trade/xswap/page.tsx
@@ -1,17 +1,13 @@
 import React from 'react';
 
-import SwapDescription from './_components/SwapDescription';
 import SwapPanel from './_components/SwapPanel';
-import TokenList from './_components/TokenList';
 
 export function TradePage() {
   return (
     <>
       <div className="flex flex-col">
         <SwapPanel />
-        <SwapDescription />
       </div>
-      <TokenList />
     </>
   );
 }

bf987b6c145abac86e52927053060ba47e4b1c19
swiftcc624 <swiftcc624@gmail.com>
2024-10-08 23:14:03 +0800

    useXConnection not working for EVM and Sui

--- apps/hswap/src/xwagmi/hooks/useXAccount.ts
+++ apps/hswap/src/xwagmi/hooks/useXAccount.ts
@@ -1,14 +1,10 @@
 import { XChainType } from '@balancednetwork/sdk-core';
-import { useCurrentAccount } from '@mysten/dapp-kit';
 import { useMemo } from 'react';
-import { useAccount } from 'wagmi';
 import { XAccount } from '../types';
 import { useXConnection } from './useXConnection';
 
 export function useXAccount(xChainType: XChainType | undefined): XAccount {
   const xConnection = useXConnection(xChainType);
-  const { address: evmAddress } = useAccount();
-  const suiAccount = useCurrentAccount();
 
   const xAccount = useMemo((): XAccount => {
     if (!xChainType) {
@@ -18,21 +14,8 @@ export function useXAccount(xChainType: XChainType | undefined): XAccount {
       };
     }
 
-    switch (xChainType) {
-      case 'EVM':
-        return {
-          address: evmAddress as string,
-          xChainType,
-        };
-      case 'SUI':
-        return {
-          address: suiAccount?.address,
-          xChainType,
-        };
-      default:
-        return xConnection?.xAccount || { address: undefined, xChainType };
-    }
-  }, [xChainType, xConnection, evmAddress, suiAccount]);
+    return xConnection?.xAccount || { address: undefined, xChainType };
+  }, [xChainType, xConnection]);
 
   return xAccount;
 }
--- apps/hswap/src/xwagmi/hooks/useXConnection.ts
+++ apps/hswap/src/xwagmi/hooks/useXConnection.ts
@@ -1,9 +1,41 @@
 import { XChainType } from '@balancednetwork/sdk-core';
 import { XConnection } from '../types';
 import { useXWagmiStore } from '../useXWagmiStore';
+import { useAccount, useConnections } from 'wagmi';
+import { useCurrentAccount, useCurrentWallet } from '@mysten/dapp-kit';
+import { useMemo } from 'react';
 
 export function useXConnection(xChainType: XChainType | undefined): XConnection | undefined {
   const xConnection = useXWagmiStore(state => (xChainType ? state.xConnections?.[xChainType] : undefined));
 
-  return xConnection;
+  const evmConnections = useConnections();
+  const { address: evmAddress } = useAccount();
+  const suiAccount = useCurrentAccount();
+  const suiCurrentWallet = useCurrentWallet();
+
+  const xConnection2 = useMemo(() => {
+    if (!xChainType) {
+      return undefined;
+    }
+
+    switch (xChainType) {
+      case 'EVM':
+        return {
+          xAccount: { address: evmAddress as string, xChainType },
+          xConnectorId: evmConnections?.[0]?.connector.id,
+        };
+      case 'SUI':
+        if (suiCurrentWallet.currentWallet && suiCurrentWallet.connectionStatus === 'connected') {
+          return {
+            xAccount: { address: suiAccount?.address, xChainType },
+            xConnectorId: suiCurrentWallet.currentWallet.name,
+          };
+        }
+        return undefined;
+      default:
+        return xConnection;
+    }
+  }, [xChainType, xConnection, evmAddress, suiAccount, evmConnections, suiCurrentWallet]);
+
+  return xConnection2;
 }

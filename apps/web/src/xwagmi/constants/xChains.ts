5ccf8b1d102a102c852f6e253a5036e63ae4dac5
hetfly <hetfly@gmail.com>
2024-10-17 15:04:57 +0200

    feat: increase stellar gas threshold

--- apps/web/src/xwagmi/constants/xChains.ts
+++ apps/web/src/xwagmi/constants/xChains.ts
@@ -304,7 +304,7 @@ export const stellar: XChain = {
     bnUSD: 'CAKU2J5T4TOMFATA56XRDZMQHNBFIOQW3JDZQNQBLNLV7LEWYTNYU6VX',
   },
   autoExecution: true,
-  gasThreshold: 1.5,
+  gasThreshold: 2,
   testnet: false,
   useXCallScanner: true,
 };

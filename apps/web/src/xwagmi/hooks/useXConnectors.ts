628ed128052d8b63ebf07b443b15ccd5ebeaf45c
swiftcc624 <swiftcc624@gmail.com>
2024-10-19 15:52:37 +0800

    connect solana wallet

--- apps/web/src/xwagmi/hooks/useXConnectors.ts
+++ apps/web/src/xwagmi/hooks/useXConnectors.ts
@@ -6,11 +6,15 @@ import { XChainType } from '../types';
 import { EvmXConnector } from '../xchains/evm';
 import { SuiXConnector } from '../xchains/sui';
 import { useXService } from './useXService';
+import { useWallet } from '@solana/wallet-adapter-react';
+import { SolanaXConnector } from '../xchains/solana';
 
 export function useXConnectors(xChainType: XChainType | undefined): XConnector[] {
   const xService = useXService(xChainType);
   const evmConnectors = useConnectors();
   const suiWallets = useWallets();
+
+  const { wallets: solanaWallets } = useWallet();
 
   const xConnectors = useMemo((): XConnector[] => {
     if (!xChainType || !xService) {
@@ -22,10 +26,14 @@ export function useXConnectors(xChainType: XChainType | undefined): XConnector[]
         return evmConnectors.map(connector => new EvmXConnector(connector));
       case 'SUI':
         return suiWallets.map(wallet => new SuiXConnector(wallet));
+      case 'SOLANA':
+        return solanaWallets
+          .filter(wallet => wallet.readyState === 'Installed')
+          .map(wallet => new SolanaXConnector(wallet));
       default:
         return xService.getXConnectors();
     }
-  }, [xService, xChainType, evmConnectors, suiWallets]);
+  }, [xService, xChainType, evmConnectors, suiWallets, solanaWallets]);
 
   return xConnectors;
 }

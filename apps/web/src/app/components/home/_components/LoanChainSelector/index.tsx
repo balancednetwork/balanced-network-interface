d7b50e29889c47eef76123e1762c5dc42a9b2c71
hetfly <hetfly@gmail.com>
2024-10-29 16:10:09 +0100

    fix: reset loan chain to collateral one on disconnect

--- apps/web/src/app/components/home/_components/LoanChainSelector/index.tsx
+++ apps/web/src/app/components/home/_components/LoanChainSelector/index.tsx
@@ -3,6 +3,8 @@ import { DropdownPopper } from '@/app/components/Popover';
 import { Typography } from '@/app/theme';
 import { NETWORK_ID } from '@/constants/config';
 import { bnUSD } from '@/constants/tokens';
+import { useSignedInWallets } from '@/hooks/useWallets';
+import { useDerivedCollateralInfo } from '@/store/collateral/hooks';
 import { useLoanActionHandlers, useLoanRecipientNetwork } from '@/store/loan/hooks';
 import { xChainMap } from '@/xwagmi/constants/xChains';
 import { XChainId } from '@/xwagmi/types';
@@ -22,12 +24,20 @@ const LoanChainSelector = ({
   const [isOpen, setOpen] = React.useState(false);
   const loanRecipientNetwork = useLoanRecipientNetwork();
   const { onAdjust: adjust, setRecipientNetwork } = useLoanActionHandlers();
+  const signedInWallets = useSignedInWallets();
+  const { sourceChain } = useDerivedCollateralInfo();
 
   const xChains = getSupportedXChainForToken(bnUSD[NETWORK_ID]);
 
   const [anchor, setAnchor] = React.useState<HTMLElement | null>(null);
 
   const arrowRef = React.useRef(null);
+
+  React.useEffect(() => {
+    if (!signedInWallets.find(wallet => wallet.xChainId === loanRecipientNetwork)) {
+      setRecipientNetwork(sourceChain);
+    }
+  }, [sourceChain, signedInWallets, loanRecipientNetwork, setRecipientNetwork]);
 
   const handleToggle = (e: React.MouseEvent<HTMLElement>) => {
     if (isOpen) {

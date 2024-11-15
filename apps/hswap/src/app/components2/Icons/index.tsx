8bc0e7d6f1aeb4fcb1895b6103720f7928776fab
0xmilktea <0xmilktea@gmail.com>
2024-11-15 18:25:01 +0800

    update tabs

--- apps/hswap/src/app/components2/Icons/index.tsx
+++ apps/hswap/src/app/components2/Icons/index.tsx
@@ -1,24 +1,33 @@
-import AllChainIcon from '@/assets/icons2/all-chain.svg';
-import AllChainWhiteIcon from '@/assets/icons2/all-chain-white.svg';
 import AllChainGradientIcon from '@/assets/icons2/all-chain-gradient.svg';
+import AllChainWhiteIcon from '@/assets/icons2/all-chain-white.svg';
+import AllChainIcon from '@/assets/icons2/all-chain.svg';
 
-import ArrowIcon from '@/assets/icons2/arrow.svg';
-import ArrowWhiteIcon from '@/assets/icons2/arrow-white.svg';
 import ArrowGradientIcon from '@/assets/icons2/arrow-gradient.svg';
+import ArrowWhiteIcon from '@/assets/icons2/arrow-white.svg';
+import ArrowIcon from '@/assets/icons2/arrow.svg';
 
 import SubtractIcon from '@/assets/icons2/subtract.svg';
 
-import ChevronDownIcon from '@/assets/icons2/chevron-down.svg';
-import ChevronDownWhiteIcon from '@/assets/icons2/chevron-down-white.svg';
 import ChevronDownGradientIcon from '@/assets/icons2/chevron-down-gradient.svg';
+import ChevronDownWhiteIcon from '@/assets/icons2/chevron-down-white.svg';
+import ChevronDownIcon from '@/assets/icons2/chevron-down.svg';
 
-import ShowIcon from '@/assets/icons2/show.svg';
 import HideIcon from '@/assets/icons2/hide.svg';
+import ShowIcon from '@/assets/icons2/show.svg';
 import ShutdownIcon from '@/assets/icons2/shutdown.svg';
 import SwitchGradientIcon from '@/assets/icons2/switch-gradient.svg';
 
-import SearchIcon from '@/assets/icons2/search.svg';
 import SearchGradientIcon from '@/assets/icons2/search-gradient.svg';
+import SearchIcon from '@/assets/icons2/search.svg';
+
+import SwapGradientIcon from '@/assets/icons2/swap-gradient.svg';
+import SwapIcon from '@/assets/icons2/swap.svg';
+
+import LimitGradientIcon from '@/assets/icons2/limit-gradient.svg';
+import LimitIcon from '@/assets/icons2/limit.svg';
+
+import DCAGradientIcon from '@/assets/icons2/dca-gradient.svg';
+import DCAIcon from '@/assets/icons2/dca.svg';
 
 export {
   AllChainIcon,
@@ -37,4 +46,10 @@ export {
   SwitchGradientIcon,
   SearchIcon,
   SearchGradientIcon,
+  SwapIcon,
+  SwapGradientIcon,
+  LimitIcon,
+  LimitGradientIcon,
+  DCAIcon,
+  DCAGradientIcon,
 };

f1266cc0139d9fb1b68d71cfeeb787b65db09890
hetfly <hetfly@gmail.com>
2024-11-19 10:49:22 +0100

    feat: add wICX contract

---
+++ packages/balanced-js/src/contracts/wICX.ts
@@ -0,0 +1,34 @@
+import { Converter as IconConverter } from 'icon-sdk-js';
+import addresses from '../addresses';
+import ContractSettings from '../contractSettings';
+import IRC2 from './IRC2';
+
+export default class wICX extends IRC2 {
+  constructor(contractSettings: ContractSettings) {
+    super(contractSettings);
+    this.address = addresses[this.nid].wicx;
+  }
+
+  transfer(to: string, value: string, data?: string, shouldConvertDataToHex: boolean = true) {
+    const payload = this.transactionParamsBuilder({
+      method: 'transfer',
+      value: IconConverter.toHexNumber(value),
+      params: {
+        _to: to,
+        _value: IconConverter.toHexNumber(value),
+        _data: data && (shouldConvertDataToHex ? IconConverter.toHex(data) : data),
+      },
+    });
+    return this.callICONPlugins(payload);
+  }
+
+  unwrap(amount: string) {
+    const payload = this.transactionParamsBuilder({
+      method: 'unwrap',
+      params: {
+        amount: IconConverter.toHexNumber(amount),
+      },
+    });
+    return this.callICONPlugins(payload);
+  }
+}

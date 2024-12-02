83f99ad009de9690abc434aae31692163fc4a8da
hetfly <hetfly@gmail.com>
2024-12-02 11:16:18 +0100

    fix: fix swapping into max stable coin amount

--- apps/web/src/store/swap/hooks.ts
+++ apps/web/src/store/swap/hooks.ts
@@ -291,7 +291,7 @@ export function useDerivedSwapInfo(): {
   }, [formattedAmounts[Field.OUTPUT], outputCurrency]);
 
   const canBridge = useMemo(() => {
-    return maximumBridgeAmount && outputCurrencyAmount ? maximumBridgeAmount?.greaterThan(outputCurrencyAmount) : true;
+    return maximumBridgeAmount && outputCurrencyAmount ? !maximumBridgeAmount.lessThan(outputCurrencyAmount) : true;
   }, [maximumBridgeAmount, outputCurrencyAmount]);
 
   return {

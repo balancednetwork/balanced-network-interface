bbecdf0a774089f2bd368d615fc80bd6a5bc082b
swiftcc624 <swiftcc624@gmail.com>
2025-01-09 00:16:46 +0800

    select the correct xchain id in LP Panel when click a pool record

--- apps/web/src/app/pages/trade/supply/_components/LiquidityDetails/PoolRecord.tsx
+++ apps/web/src/app/pages/trade/supply/_components/LiquidityDetails/PoolRecord.tsx
@@ -1,42 +1,26 @@
-import React, { useState } from 'react';
+import React from 'react';
 
-import { BalancedJs } from '@balancednetwork/balanced-js';
 import { Pair } from '@balancednetwork/v1-sdk';
 import { Trans } from '@lingui/macro';
-import { Accordion } from '@reach/accordion';
 import BigNumber from 'bignumber.js';
-import { AnimatePresence, motion } from 'framer-motion';
-import { omit } from 'lodash-es';
 import { useMedia } from 'react-use';
 import { Flex } from 'rebass/styled-components';
 import styled from 'styled-components';
 
-import Message from '@/app/Message';
 import { Typography } from '@/app/theme';
 import ArrowDownIcon from '@/assets/icons/arrow-line.svg';
-import { MINIMUM_B_BALANCE_TO_SHOW_POOL } from '@/constants/index';
-import { BIGINT_ZERO } from '@/constants/misc';
-import { BalanceData, Pool, useBalance, usePoolTokenAmounts, useSuppliedTokens } from '@/hooks/useV2Pairs';
-import { PairData, useAllPairsById } from '@/queries/backendv2';
-import { Source, useBBalnAmount, useSources, useTotalSupply } from '@/store/bbaln/hooks';
-import { useTokenListConfig } from '@/store/lists/hooks';
+import { Pool, useBalance, usePoolTokenAmounts } from '@/hooks/useV2Pairs';
+import { PairData } from '@/queries/backendv2';
+import { Source, useBBalnAmount, useTotalSupply } from '@/store/bbaln/hooks';
 import { useMintActionHandlers } from '@/store/mint/hooks';
 import { Field } from '@/store/mint/reducer';
-import { useRewards } from '@/store/reward/hooks';
 import { useStakedLPPercent, useWithdrawnPercent } from '@/store/stakedLP/hooks';
 
-import { QuestionWrapper } from '@/app/components/QuestionHelper';
 import Skeleton from '@/app/components/Skeleton';
-import { MouseoverTooltip } from '@/app/components/Tooltip';
-import QuestionIcon from '@/assets/icons/question.svg';
 import { formatBigNumber } from '@/utils';
 import { getFormattedNumber } from '@/utils/formatter';
-import { XChainId } from '@balancednetwork/sdk-core';
 import { getFormattedRewards, stakedFraction, totalSupply } from '../utils';
-import { WithdrawPanel, WithdrawPanelQ, getABBalance, getShareReward } from './WithdrawPanel';
-import { StyledBoxPanel } from './shared';
-
-const TableWrapper = styled.div``;
+import { getShareReward } from './WithdrawPanel';
 
 const DashGrid = styled.div`
   display: grid;
@@ -140,16 +124,12 @@ export const PoolRecord = ({
   const baseSupplyAmount = totalSupply(baseWithdrawValue, baseAmount);
   const quoteSupplyAmount = totalSupply(quoteWithdrawValue, quoteAmount);
 
-  const { onCurrencySelection } = useMintActionHandlers(false);
+  const { onCurrencySelection, onChainSelection } = useMintActionHandlers(false);
 
   const handlePoolClick = () => {
-    if (pairData) {
-      onCurrencySelection(Field.CURRENCY_A, pairData.info.baseToken);
-      onCurrencySelection(Field.CURRENCY_B, pairData.info.quoteToken);
-    } else {
-      onCurrencySelection(Field.CURRENCY_A, pair.reserve0.currency);
-      onCurrencySelection(Field.CURRENCY_B, pair.reserve1.currency);
-    }
+    onCurrencySelection(Field.CURRENCY_A, pair.reserve0.currency);
+    onCurrencySelection(Field.CURRENCY_B, pair.reserve1.currency);
+    onChainSelection(Field.CURRENCY_A, xChainId);
   };
 
   return (

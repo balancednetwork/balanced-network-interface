6ca7a13c29932c8f99ca66d8d1e2bc85be4910ef
swiftcc624 <swiftcc624@gmail.com>
2025-01-14 22:28:33 +0800

    don't persist useXChainHeightStore

--- packages/xwagmi/src/xcall/zustand/useXChainHeightStore.tsx
+++ packages/xwagmi/src/xcall/zustand/useXChainHeightStore.tsx
@@ -6,8 +6,6 @@ import { create } from 'zustand';
 import { immer } from 'zustand/middleware/immer';
 
 import { XChain, XChainId } from '@/types';
-import { jsonStorageOptions } from '@/utils';
-import { createJSONStorage, persist } from 'zustand/middleware';
 
 type XChainHeightStore = {
   xChainHeights: Partial<Record<XChainId, bigint>>;
@@ -16,25 +14,19 @@ type XChainHeightStore = {
 };
 
 export const useXChainHeightStore = create<XChainHeightStore>()(
-  persist(
-    immer((set, get) => ({
-      xChainHeights: {},
+  immer((set, get) => ({
+    xChainHeights: {},
 
-      getXChainHeight: (xChainId: XChainId) => {
-        const height = get().xChainHeights?.[xChainId];
-        return height ? BigInt(height) : BigInt(0);
-      },
-      setXChainHeight: (xChainId: XChainId, height: bigint) => {
-        set(state => {
-          state.xChainHeights[xChainId] = height;
-        });
-      },
-    })),
-    {
-      name: 'xChainHeight-store',
-      storage: createJSONStorage(() => localStorage, jsonStorageOptions),
+    getXChainHeight: (xChainId: XChainId) => {
+      const height = get().xChainHeights?.[xChainId];
+      return height ? BigInt(height) : BigInt(0);
     },
-  ),
+    setXChainHeight: (xChainId: XChainId, height: bigint) => {
+      set(state => {
+        state.xChainHeights[xChainId] = height;
+      });
+    },
+  })),
 );
 
 export const xChainHeightActions = {

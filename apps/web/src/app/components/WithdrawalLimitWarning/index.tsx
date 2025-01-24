0374485afabea8e0444dbc928873a5d6977f23ab
hetfly <hetfly@gmail.com>
2025-01-24 05:46:35 +0100

    feat: add tooltip to withdrawal limit and improve content

--- apps/web/src/app/components/WithdrawalLimitWarning/index.tsx
+++ apps/web/src/app/components/WithdrawalLimitWarning/index.tsx
@@ -1,14 +1,17 @@
-import React from 'react';
+import React, { useMemo } from 'react';
 
 import { Currency, CurrencyAmount } from '@balancednetwork/sdk-core';
-import { Trans } from '@lingui/macro';
+import { Trans, t } from '@lingui/macro';
 import { Flex } from 'rebass/styled-components';
 
 import { UnderlineText } from '@/app/components/DropdownText';
 import { Typography } from '@/app/theme';
 import { useOraclePrices } from '@/store/oracle/hooks';
-import { formatBalance } from '@/utils/formatter';
+
+import QuestionIcon from '@/assets/icons/question.svg';
+import { useWithdrawalsFloorDEXData } from '@/store/swap/hooks';
 import BigNumber from 'bignumber.js';
+import { MouseoverTooltip } from '../Tooltip';
 
 interface WithdrawalLimitWarningProps {
   limitAmount: CurrencyAmount<Currency>;
@@ -19,6 +22,12 @@ const BridgeLimitWarning: React.FC<WithdrawalLimitWarningProps> = props => {
   const { limitAmount, onLimitAmountClick } = props;
   const prices = useOraclePrices();
   const price = prices[limitAmount.currency.symbol];
+  const { data: withdrawalLimits } = useWithdrawalsFloorDEXData();
+
+  const limitData = useMemo(() => {
+    const limit = withdrawalLimits?.find(limit => limit.token.symbol === limitAmount.currency.symbol);
+    return limit;
+  }, [withdrawalLimits, limitAmount.currency.symbol]);
 
   return (
     <Flex alignItems="center" justifyContent="center" mt={2}>
@@ -41,8 +50,14 @@ const BridgeLimitWarning: React.FC<WithdrawalLimitWarningProps> = props => {
             </UnderlineText>{' '}
           </>
         }
-
-        <Trans>available. Limit hit TODO</Trans>
+        <Trans>available. </Trans>{' '}
+        <MouseoverTooltip
+          placement="bottom"
+          width={300}
+          text={t`Only ${limitData?.percentageFloor.times(100).toFixed(2).replace('.00', '')}% can be withdrawn at a time. Limit will be increased gradually over the next ${limitData?.floorTimeDecayInHours.toFixed(0)} hours.`}
+        >
+          <QuestionIcon width={14} style={{ cursor: 'help', marginTop: -2 }} />
+        </MouseoverTooltip>
       </Typography>
     </Flex>
   );

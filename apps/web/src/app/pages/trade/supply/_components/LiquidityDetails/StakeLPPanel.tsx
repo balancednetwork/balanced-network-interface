a5ab14c7fa9af36eeea4ff2fbfd5247ebcefc59f
0xmilktea <0xmilktea@gmail.com>
2025-01-31 20:44:51 +0800

    refactor RespoRewardInfo component

--- apps/web/src/app/pages/trade/supply/_components/LiquidityDetails/StakeLPPanel.tsx
+++ apps/web/src/app/pages/trade/supply/_components/LiquidityDetails/StakeLPPanel.tsx
@@ -1,31 +1,22 @@
 import React, { useEffect, useCallback, useMemo } from 'react';
 
 import Nouislider from '@/packages/nouislider-react';
-import { Trans } from '@lingui/macro';
 import BigNumber from 'bignumber.js';
 import { useMedia } from 'react-use';
 import { Box, Flex } from 'rebass/styled-components';
 
 import { Button, TextButton } from '@/app/components/Button';
-import Skeleton from '@/app/components/Skeleton';
 import { Typography } from '@/app/theme';
 import { SLIDER_RANGE_MAX_BOTTOM_THRESHOLD, ZERO } from '@/constants/index';
 import { Pool, usePoolTokenAmounts } from '@/hooks/useV2Pairs';
-import { useAllPairsById } from '@/queries/backendv2';
-import { useIncentivisedPairs, useRatesWithOracle } from '@/queries/reward';
-import { useBBalnAmount, useSources, useTotalSupply } from '@/store/bbaln/hooks';
-import { useRewards } from '@/store/reward/hooks';
+import { useIncentivisedPairs } from '@/queries/reward';
 import { useChangeStakedLPPercent, useStakedLPPercent } from '@/store/stakedLP/hooks';
 import { formatBigNumber } from '@/utils';
-import { formatValue } from '@/utils/formatter';
-import { getFormattedExternalRewards, getFormattedRewards, stakedFraction } from '../utils';
+import RespoRewardsInfo from './RespoRewardsInfo';
 import StakeLPModal from './StakeLPModal';
-import { getExternalShareReward, getShareReward } from './WithdrawPanel';
 
 export default function StakeLPPanel({ pool }: { pool: Pool }) {
-  const { poolId, pair, balance: lpBalance, stakedLPBalance } = pool;
-  const sources = useSources();
-  const { data: allPairs } = useAllPairsById();
+  const { poolId, balance: lpBalance, stakedLPBalance } = pool;
   const { data: incentivisedPairs } = useIncentivisedPairs();
 
   const stakedBalance = useMemo(() => new BigNumber(stakedLPBalance?.toFixed() || 0), [stakedLPBalance]);
@@ -79,25 +70,8 @@ export default function StakeLPPanel({ pool }: { pool: Pool }) {
   const afterAmount = stakedPercent.multipliedBy(totalStaked).div(100);
 
   const upSmall = useMedia('(min-width: 800px)');
-  const prices = useRatesWithOracle();
-  const rewards = useRewards();
   const [aBalance, bBalance] = usePoolTokenAmounts(pool);
   const pairName = `${aBalance.currency.symbol || '...'}/${bBalance.currency.symbol || '...'}`;
-  const sourceName = pairName === 'sICX/BTCB' ? 'BTCB/sICX' : pairName;
-  const totalReward = rewards[sourceName];
-  const stakedFractionValue = stakedFraction(stakedPercent);
-  const totalBbaln = useTotalSupply();
-  const userBbaln = useBBalnAmount();
-
-  // TODO: understand and rewrite to support crosschain
-  const reward = getShareReward(
-    totalReward,
-    sources && sources[sourceName],
-    pool,
-    stakedFractionValue,
-    totalBbaln,
-    userBbaln,
-  );
 
   const isIncentivised = useMemo(
     () =>
@@ -106,84 +80,9 @@ export default function StakeLPPanel({ pool }: { pool: Pool }) {
     [incentivisedPairs, pairName],
   );
 
-  const pairData = useMemo(() => {
-    return allPairs?.[pair.poolId!];
-  }, [allPairs, pair.poolId]);
-
-  const externalRewards = useMemo(() => {
-    if (!isIncentivised || !pairData) {
-      return [];
-    }
-    return pairData.externalRewards || [];
-  }, [isIncentivised, pairData]);
-
-  const RespoRewardsInfo = () => {
-    // biome-ignore lint/correctness/useExhaustiveDependencies: <explanation>
-    const totalAPR = useMemo(() => {
-      let balnAndFeesAPR = new BigNumber(0);
-      if (allPairs && sources) {
-        balnAndFeesAPR = sources[sourceName].balance.isZero()
-          ? new BigNumber(allPairs[poolId].feesApy).times(100)
-          : new BigNumber(allPairs[pair.poolId!].balnApy)
-              .times(sources[sourceName].workingBalance.dividedBy(sources[sourceName].balance))
-              .plus(allPairs[poolId].feesApy)
-              .times(100);
-      }
-
-      let totalAPR = balnAndFeesAPR;
-      if (externalRewards.length > 0 && allPairs && allPairs[poolId]?.externalRewardsTotalAPR) {
-        totalAPR = totalAPR.plus(allPairs[poolId]?.externalRewardsTotalAPR);
-      }
-
-      return totalAPR;
-    }, [pair, allPairs, sources, sourceName, poolId]);
-
-    return (
-      <Flex
-        marginBottom={4}
-        justifyContent="space-between"
-        paddingBottom={4}
-        sx={{ borderBottom: '1px solid rgba(255, 255, 255, 0.15)' }}
-      >
-        <Box>
-          <Typography color="text2">
-            <Trans>Daily rewards</Trans>
-          </Typography>
-          <Typography color="text" fontSize={16}>
-            {getFormattedRewards(reward, externalRewards.length === 0)}
-          </Typography>
-          {externalRewards.map(reward => {
-            const rewardPrice = prices?.[reward.currency.wrapped.symbol];
-            const rewardShare = getExternalShareReward(reward, pool, stakedFractionValue, pairData?.stakedLP);
-            return (
-              <Typography key={reward.currency.symbol} color="text" fontSize={16}>
-                {getFormattedExternalRewards(rewardShare, rewardPrice?.toFixed())}
-              </Typography>
-            );
-          })}
-        </Box>
-
-        <Box sx={{ textAlign: 'right' }}>
-          <Typography color="text2">
-            <Trans>APR</Trans>
-          </Typography>
-          <Typography color="text" fontSize={16}>
-            {!allPairs || !sources ? (
-              <Skeleton width={100}></Skeleton>
-            ) : sources[sourceName] ? (
-              `${formatValue(totalAPR.toFixed(2), false)}%`
-            ) : (
-              '-'
-            )}
-          </Typography>
-        </Box>
-      </Flex>
-    );
-  };
-
   return (
     <Box width={upSmall ? 1 / 2 : 1}>
-      {!upSmall && <RespoRewardsInfo />}
+      {!upSmall && <RespoRewardsInfo pool={pool} />}
       <Typography variant="h3" marginBottom="15px">
         Stake LP tokens
       </Typography>

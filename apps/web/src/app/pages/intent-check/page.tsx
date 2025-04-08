4c6daa0e42867edb9ae171e48829222be09a5aff
hetfly <hetfly@gmail.com>
2025-04-08 11:24:25 +0200

    feat: add order check page

---
+++ apps/web/src/app/pages/intent-check/page.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Flex } from 'rebass/styled-components';
+
+export function IntentCheckPage() {
+  return (
+    <Flex flexDirection="column" width="100%">
+      {/* Content will be added here */}kk
+    </Flex>
+  );
+}

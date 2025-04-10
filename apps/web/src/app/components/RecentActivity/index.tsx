283768bc3ad0941b3246ead6edb9fa589d48a307
hetfly <hetfly@gmail.com>
2025-04-10 11:16:50 +0200

    feat: update dropdown layout

--- apps/web/src/app/components/RecentActivity/index.tsx
+++ apps/web/src/app/components/RecentActivity/index.tsx
@@ -1,24 +1,45 @@
+import { Typography } from '@/app/theme';
 import { useCombinedTransactions } from '@/hooks/useCombinedTransactions';
 import React from 'react';
+import styled from 'styled-components';
+
+const Wrap = styled.div`
+  padding: 25px 0;
+  width: 400px;
+  max-width: calc(100vw - 4px);
+
+  ul {
+    max-height: 500px;
+    overflow-y: auto;
+    list-style: none;
+    padding: 0 25px;
+    margin-bottom: 0;
+  }
+`;
+
+const ListItem = styled.li`
+`;
 
 const RecentActivity: React.FC = () => {
   const { transactions, isMMTransaction } = useCombinedTransactions();
 
   return (
-    <div>
-      <h3 className="text-lg font-semibold mb-2">Recent Activity</h3>
+    <Wrap>
+      <Typography variant="h2" px="25px">
+        Recent Activity
+      </Typography>
       {transactions.length === 0 ? (
         <div>No activity to display.</div>
       ) : (
         <ul>
           {transactions.map(transaction => (
-            <li key={transaction.id}>
+            <ListItem key={transaction.id}>
               {isMMTransaction(transaction) ? 'MM Transaction' : 'X Transaction'} - {transaction.id.slice(0, 6)}...
-            </li>
+            </ListItem>
           ))}
         </ul>
       )}
-    </div>
+    </Wrap>
   );
 };
 

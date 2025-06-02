f682ecbf1fa701b9d075d311daf21cc60c257978
hetfly <hetfly@gmail.com>
2025-06-02 15:43:40 +0200

    feat: improve tx ux

--- apps/web/src/hooks/useWalletPrompting.ts
+++ apps/web/src/hooks/useWalletPrompting.ts
@@ -1,13 +1,22 @@
-import { useCallback, useState } from 'react';
+import { useCallback, useRef, useState } from 'react';
 
 export const useWalletPrompting = () => {
   const [isWalletPrompting, setIsWalletPrompting] = useState(false);
+  const timeoutRef = useRef<NodeJS.Timeout>();
 
   const setWalletPrompting = useCallback((value: boolean) => {
+    // Clear any existing timeout
+    if (timeoutRef.current) {
+      clearTimeout(timeoutRef.current);
+      timeoutRef.current = undefined;
+    }
+
     setIsWalletPrompting(value);
+
     if (value) {
-      setTimeout(() => {
+      timeoutRef.current = setTimeout(() => {
         setIsWalletPrompting(false);
+        timeoutRef.current = undefined;
       }, 30000);
     }
   }, []);

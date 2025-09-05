65938c6dbc07563bfc55143216f0fb4101715233
hetfly <hetfly@gmail.com>
2025-09-05 16:43:29 +0200

    fix: fix legacy trading routing

--- apps/web/src/app/pages/trade/xswap/page-legacy.tsx
+++ apps/web/src/app/pages/trade/xswap/page-legacy.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 
 import { SectionPanel } from '@/app/components/Panel';
-import SwapDescriptionLegacy from './_components/SwapDescriptionLegacy';
 import SwapPanelLegacy from './_components/SwapPanelLegacy';
 import TokenList from './_components/TokenList';
 

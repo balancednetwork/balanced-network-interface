5d1e22a1ad50609f0c4e48c1b228263fbc87291b
hetfly <hetfly@gmail.com>
2025-09-16 12:58:31 +0200

    fix: fix bnUSD(old) price in currency select

--- apps/web/src/hooks/useSortCurrency.ts
+++ apps/web/src/hooks/useSortCurrency.ts
@@ -29,7 +29,7 @@ const getXCurrencyBalance = (
     if (isXToken(currency)) {
       return SUPPORTED_XCALL_CHAINS.reduce((sum, xChainId) => {
         if (xBalances[xChainId]) {
-          const tokenAddress = getXTokenAddress(xChainId, currency.symbol);
+          const tokenAddress = getXTokenAddress(xChainId, currency.symbol.replace('(old)', ''));
           const balance = new BigNumber(xBalances[xChainId]?.[tokenAddress ?? -1]?.toFixed() || 0);
           sum = sum.plus(balance);
         }
@@ -88,16 +88,18 @@ export default function useSortCurrency(initialState: SortingType, selectedChain
         dataToSort.sort((a, b) => {
           const aBalance = getXCurrencyBalance(xBalances, a, selectedChainId) || new BigNumber(0);
           const bBalance = getXCurrencyBalance(xBalances, b, selectedChainId) || new BigNumber(0);
-          const aValue = aBalance.times(new BigNumber(rateFracs[a.symbol]?.toFixed(8) || '0'));
-          const bValue = bBalance.times(new BigNumber(rateFracs[b.symbol]?.toFixed(8) || '0'));
+          const aValue = aBalance.times(new BigNumber(rateFracs[a.symbol.replace('(old)', '')]?.toFixed(8) || '0'));
+          const bValue = bBalance.times(new BigNumber(rateFracs[b.symbol.replace('(old)', '')]?.toFixed(8) || '0'));
           return aValue.isGreaterThan(bValue) ? -1 * direction : 1 * direction;
         });
       }
 
       if (sortBy.key === 'price') {
         dataToSort.sort((a, b) => {
-          if (!rateFracs[a.symbol] || !rateFracs[b.symbol]) return 0;
-          return rateFracs[a.symbol].greaterThan(rateFracs[b.symbol]) ? -1 * direction : 1 * direction;
+          if (!rateFracs[a.symbol.replace('(old)', '')] || !rateFracs[b.symbol.replace('(old)', '')]) return 0;
+          return rateFracs[a.symbol.replace('(old)', '')].greaterThan(rateFracs[b.symbol.replace('(old)', '')])
+            ? -1 * direction
+            : 1 * direction;
         });
       }
 

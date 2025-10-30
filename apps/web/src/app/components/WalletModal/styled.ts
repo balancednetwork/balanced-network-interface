2866144f804bd9a9980cda5e3ccf2a2bbaa1a661
hetfly <hetfly@gmail.com>
2025-10-30 18:09:03 +0100

    content: inprove evm chainlist in wallet modal

--- apps/web/src/app/components/WalletModal/styled.ts
+++ apps/web/src/app/components/WalletModal/styled.ts
@@ -175,11 +175,11 @@ export const XChainsWrap = styled.div<{ signedIn: boolean }>`
   `};
 
   img {
-    margin: 5px 6px;
+    margin: 5px 7px;
   }
 
   @media screen and (min-width: ${mobileWalletBreakpoint}) {
-    max-width: 236px;
+    max-width: 197px;
     margin-bottom: 0;
   }
 `;
